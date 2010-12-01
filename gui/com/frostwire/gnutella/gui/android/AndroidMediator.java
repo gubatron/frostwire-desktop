73ee9d7040fd385630f77817f8c93e97c573ef0d
gubatron <gubatron@gmail.com>
2010-12-01 05:52:02 -0500

    File transfers and the two explorers are now inside a Split Pane. Played a lot with the layouts containing the Explorer only to go back to Gridbag layout.

--- gui/com/frostwire/gnutella/gui/android/AndroidMediator.java
+++ gui/com/frostwire/gnutella/gui/android/AndroidMediator.java
@@ -4,10 +4,14 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JSplitPane;
 
 import com.limegroup.gnutella.gui.GUIMediator;
 import com.limegroup.gnutella.gui.I18n;
@@ -35,6 +39,10 @@ public class AndroidMediator implements ThemeObserver {
 	private ProgressPanel PROGRESS_PANEL;
 	
 	private TaskProcessor ACTIVITY_PROCESSOR;
+
+	private JPanel EXPLORERS_CONTAINER;
+
+	private JSplitPane SPLIT_PANE;
     
 	/**
 	 * @return the <tt>AndroidMediator</tt> instance
@@ -61,31 +69,41 @@ public class AndroidMediator implements ThemeObserver {
 		
 		MAIN_PANEL.add(DEVICE_BAR, BorderLayout.PAGE_START);
 		
-		JPanel center = new JPanel(new GridBagLayout());
+		//up and down
+		SPLIT_PANE = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
+		SPLIT_PANE.setAutoscrolls(true);
+		SPLIT_PANE.setDividerSize(4);
+
 		
-		GridBagConstraints c;
+		EXPLORERS_CONTAINER = new JPanel(new GridBagLayout());
+
+		GridBagConstraints c = new GridBagConstraints();
+		c.gridx=0;
+		c.gridy=0;
+		c.fill=GridBagConstraints.BOTH;
+		c.weightx = 0.9;
+		c.weighty=1.0;
+		c.anchor=GridBagConstraints.NORTHWEST;
+		
+		DESKTOP_EXPLORER.setMinimumSize(new Dimension(700,700));
+
+		EXPLORERS_CONTAINER.add(DESKTOP_EXPLORER, c);
 		
 		c = new GridBagConstraints();
-		c.gridx = 0;
-		c.gridy = 0;
-		c.fill = GridBagConstraints.BOTH;
-		c.weightx = 0.7;
-		c.weighty = 10;
-		center.add(DESKTOP_EXPLORER, c);
-		
-		c = new GridBagConstraints();
-		c.gridx = 1;
-        c.gridy = 0;
-		c.fill = GridBagConstraints.VERTICAL;
-		c.anchor = GridBagConstraints.EAST;
-		//c.gridwidth = GridBagConstraints.REMAINDER;
-		c.weightx = 0.3;
-		c.weighty = 10;
-		center.add(DEVICE_EXPLORER, c);
-		
-		MAIN_PANEL.add(center, BorderLayout.CENTER);
-		
-		MAIN_PANEL.add(PROGRESS_PANEL, BorderLayout.PAGE_END);
+		c.gridx=GridBagConstraints.RELATIVE;
+		c.gridy=0;
+		c.weightx=0;
+		c.gridwidth=GridBagConstraints.REMAINDER;
+		c.fill=GridBagConstraints.VERTICAL;
+		c.anchor=GridBagConstraints.EAST;
+		c.weightx = 0.1;
+		c.weighty=1.0;
+		EXPLORERS_CONTAINER.add(DEVICE_EXPLORER,c);
+
+		SPLIT_PANE.add(EXPLORERS_CONTAINER);
+		SPLIT_PANE.add(PROGRESS_PANEL);
+
+		MAIN_PANEL.add(SPLIT_PANE,BorderLayout.CENTER);
 	}
 
 	@Override
