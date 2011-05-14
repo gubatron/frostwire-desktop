6257628451edbe326889a6ea0d2c0e6c965f8908
gubatron <gubatron@gmail.com>
2011-05-14 16:13:25 -0400

    Version bump to FrostWire 5.0.0

--- core/com/limegroup/gnutella/util/FrostWireUtils.java
+++ core/com/limegroup/gnutella/util/FrostWireUtils.java
@@ -28,7 +28,7 @@ public final class FrostWireUtils {
 	 * Constant for the current version of FrostWire.
 	 */
     @InspectablePrimitive("frostwire version")
-	private static final String FROSTWIRE_VERSION = "4.21.6";
+	private static final String FROSTWIRE_VERSION = "5.0.0";
     
     /**
      * The current revision of the BitTorrent protocol implementation.
