a59fde603d1e37eabf49c2f148af8fb93e9b0dae
gubatron <gubatron@gmail.com>
2011-06-13 20:58:00 -0400

    Refactored Filter Panel Ranged Sliders. Created new component "LabeledRangeSlider", a RangeSlider with Title and labels for minimum and max. Inherits from JPanel and composes the RangeSlider functionality. DRY Principle. The FilterPanel uses a simple GridbagLayout now.

--- gui/com/limegroup/gnutella/gui/search/FilterPanel.java
+++ gui/com/limegroup/gnutella/gui/search/FilterPanel.java
@@ -1,22 +1,17 @@
 package com.limegroup.gnutella.gui.search;
 
-import java.awt.BorderLayout;
-import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
-import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SwingConstants;
 import javax.swing.border.TitledBorder;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
-import com.frostwire.gui.components.RangeSlider;
+import com.frostwire.gui.components.LabeledRangeSlider;
 import com.limegroup.gnutella.gui.I18n;
 
 public class FilterPanel extends JPanel {
@@ -26,14 +21,8 @@ public class FilterPanel extends JPanel {
      */
     private static final long serialVersionUID = 346186974515196119L;
     
-    private JLabel _labelMinSeeds;
-    private JLabel _labelSeeds;
-    private JLabel _labelMaxSeeds;
-    private RangeSlider _rangeSliderSeeds;
-    private JLabel _labelMinSize;
-    private JLabel _labelSize;
-    private JLabel _labelMaxSize;
-    private RangeSlider _rangeSliderSize;
+    private LabeledRangeSlider _rangeSliderSeeds;
+    private LabeledRangeSlider _rangeSliderSize;
 
     private int _minSeeds;
     private int _maxSeeds;
@@ -51,62 +40,31 @@ public class FilterPanel extends JPanel {
     }
 
     protected void setupUI() {
-        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-        setBorder(new TitledBorder(I18n.tr("Filter")));
+        setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 1.0;
+        c.gridwidth = GridBagConstraints.REMAINDER;
         
-        JPanel panelSeeds = new JPanel(new BorderLayout());
+        setBorder(new TitledBorder(I18n.tr("Filter Results")));
 
-        _labelMinSeeds = new JLabel(I18n.tr("0"));
-        _labelMinSeeds.setBorder(BorderFactory.createEmptyBorder(0, 6, 0, 0));
-        panelSeeds.add(_labelMinSeeds, BorderLayout.LINE_START);
-        
-        _labelSeeds = new JLabel(I18n.tr("Sources"));
-        _labelSeeds.setBorder(BorderFactory.createRaisedBevelBorder());
-        add(_labelSeeds);
-        
-        _labelMaxSeeds = new JLabel(I18n.tr("Max"));
-        _labelMaxSeeds.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 6));
-        panelSeeds.add(_labelMaxSeeds, BorderLayout.LINE_END);
-        
-        _rangeSliderSeeds = new RangeSlider(0, 1000);
+        _rangeSliderSeeds = new LabeledRangeSlider("Download Sources", null, 0, 50000);
         _rangeSliderSeeds.setPreferredSize(new Dimension(80, (int) _rangeSliderSeeds.getPreferredSize().getHeight()));
-        _rangeSliderSeeds.setValue(0);
-        _rangeSliderSeeds.setUpperValue(1000);
         _rangeSliderSeeds.addChangeListener(new ChangeListener() {
             public void stateChanged(ChangeEvent e) {
                 rangeSliderSeeds_stateChanged(e);
             }
         });
-        panelSeeds.add(_rangeSliderSeeds, BorderLayout.PAGE_START);
-        
-        add(panelSeeds);
-        
-        JPanel panelSize = new JPanel(new BorderLayout());
+        add(_rangeSliderSeeds,c);
 
-        _labelMinSize = new JLabel(I18n.tr("0"));
-        _labelMinSize.setBorder(BorderFactory.createEmptyBorder(0, 6, 0, 0));
-        panelSize.add(_labelMinSize, BorderLayout.LINE_START);
-        
-        _labelSize = new JLabel(I18n.tr("Size"));
-        _labelSize.setBorder(BorderFactory.createRaisedBevelBorder());
-        add(_labelSize);
-        
-        _labelMaxSize = new JLabel(I18n.tr("Max"));
-        _labelMaxSize.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 6));
-        panelSize.add(_labelMaxSize, BorderLayout.LINE_END);
-        
-        _rangeSliderSize = new RangeSlider(0, 1000);
+        _rangeSliderSize = new LabeledRangeSlider("File Size",null,0,1000);
         _rangeSliderSize.setPreferredSize(new Dimension(80, (int) _rangeSliderSize.getPreferredSize().getHeight()));
-        _rangeSliderSize.setValue(0);
-        _rangeSliderSize.setUpperValue(1000);
         _rangeSliderSize.addChangeListener(new ChangeListener() {
             public void stateChanged(ChangeEvent e) {
                 rangeSliderSize_stateChanged(e);
             }
         });
-        panelSize.add(_rangeSliderSize, BorderLayout.PAGE_START);
-        
-        add(panelSize);
+        add(_rangeSliderSize,c);
     }
     
     protected void rangeSliderSeeds_stateChanged(ChangeEvent e) {
