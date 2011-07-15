f0c868f570cd545d446401bfc10ecad151abbd53
gubatron <gubatron@gmail.com>
2011-07-15 15:42:46 -0400

    Up to 50 characters are now allowed on a search. New warning shown to the user before Rebuildint their FrostWire iTunes Playlist. Changelog update for 5.0.7.

--- gui/com/limegroup/gnutella/gui/menu/ToolsMenu.java
+++ gui/com/limegroup/gnutella/gui/menu/ToolsMenu.java
@@ -2,6 +2,8 @@ package com.limegroup.gnutella.gui.menu;
 
 import java.awt.event.ActionEvent;
 
+import javax.swing.JOptionPane;
+
 import org.limewire.i18n.I18nMarker;
 import org.limewire.util.OSUtils;
 
@@ -44,17 +46,22 @@ final class ToolsMenu extends AbstractMenu {
 		
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
-			iTunesMediator.instance().resetFrostWirePlaylist();
+			int result = JOptionPane.showConfirmDialog(GUIMediator.getAppFrame(), 
+					I18n.tr("This will remove your \"FrostWire\" playlist in iTunes and replace\n" +
+							"it with one containing all the iTunes compatible files in your \n" +
+							"Frostwire \"Torrent Data Folder\"\n\n" +
+							"Please note that it will add the files to the iTunes library as well\n" +
+							"and this could result in duplicate files on your iTunes library\n\n" +
+							"Are you sure you want to continue?"), 
+							I18n.tr("Warning"), JOptionPane.WARNING_MESSAGE, JOptionPane.YES_OPTION | JOptionPane.CANCEL_OPTION);
+			
+			if (result==JOptionPane.YES_OPTION) {
+				iTunesMediator.instance().resetFrostWirePlaylist();
+			}
 		}
-		
 	}
 	
-	
 	private static class ShowOptionsAction extends AbstractAction {
-	    
-	    /**
-         * 
-         */
         private static final long serialVersionUID = 6187597973189408647L;
 
         public ShowOptionsAction() {
