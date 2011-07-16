89cb9f06fa1fa390b07c8a7cdc7141159605dea2
aldenml <aldenml@gmail.com>
2011-07-16 16:49:21 -0400

    More in library db layer.

--- components/alexandria/src/main/java/com/frostwire/alexandria/Library.java
+++ components/alexandria/src/main/java/com/frostwire/alexandria/Library.java
@@ -1,12 +1,54 @@
 package com.frostwire.alexandria;
 
 import java.io.File;
+import java.util.List;
+
+import com.frostwire.alexandria.db.LibraryDB;
 
 public class Library {
 
-    private final File _libraryFile;
+    private final LibraryDB _db;
+
+    private boolean _closed;
     
+    private String _name;
+    private int _version;
+
     public Library(File libraryFile) {
-        _libraryFile = libraryFile;
+        _db = new LibraryDB(libraryFile);
+        
+        fillLibraryInfo();
+    }
+
+    public File getLibraryFile() {
+        return _db.getDatabaseFile();
+    }
+
+    public void close() {
+        if (_closed) {
+            return;
+        }
+
+        _closed = true;
+        _db.close();
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        try {
+            close();
+        } finally {
+            super.finalize();
+        }
+    }
+    
+    private void fillLibraryInfo() {
+        List<List<Object>> result = _db.query("SELECT name, version FROM LibraryInfo");
+    }
+
+    public static void main(String[] args) {
+        File dbFile = new File("/home/atorres/Downloads/testalex/testdb");
+        Library library = new Library(dbFile);
+        library.close();
     }
 }
--- components/alexandria/src/main/java/com/frostwire/alexandria/db/LibraryDB.java
+++ components/alexandria/src/main/java/com/frostwire/alexandria/db/LibraryDB.java
@@ -13,7 +13,10 @@ import java.util.List;
 
 public class LibraryDB {
 
+    private static final int LIBRARY_DATABASE_VERSION = 1;
+
     private final File _databaseFile;
+    private final String _name;
 
     private final Connection _connection;
 
@@ -27,7 +30,15 @@ public class LibraryDB {
 
     public LibraryDB(File databaseFile) {
         _databaseFile = databaseFile;
-        _connection = openOrCreateDatabase();
+
+        File path = databaseFile;
+        _name = databaseFile.getName();
+
+        _connection = openOrCreateDatabase(path, _name);
+    }
+
+    public File getDatabaseFile() {
+        return _databaseFile;
     }
 
     public synchronized List<List<Object>> query(String expression) {
@@ -55,6 +66,10 @@ public class LibraryDB {
         return new ArrayList<List<Object>>();
     }
 
+    public synchronized boolean update(String expression) {
+        return update(_connection, expression);
+    }
+
     public void close() {
         try {
             Statement statement = _connection.createStatement();
@@ -65,11 +80,11 @@ public class LibraryDB {
         }
     }
 
-    private Connection openConnection(boolean createIfNotExists) {
+    private Connection openConnection(File path, String name, boolean createIfNotExists) {
         try {
             StringBuilder sb = new StringBuilder();
             sb.append("jdbc:hsqldb:file:");
-            sb.append(_databaseFile.getAbsolutePath());
+            sb.append(new File(path, name).getAbsolutePath());
 
             if (!createIfNotExists) {
                 sb.append(";ifexists=true");
@@ -80,16 +95,39 @@ public class LibraryDB {
         }
     }
 
-    private Connection createDatabase() {
-        Connection connection = openConnection(true);
+    private Connection createDatabase(File path, String name) {
+        Connection connection = openConnection(path, name, true);
+
+        // STRUCTURE CREATION
+
+        update(connection, "CREATE TABLE LibraryInfo (name VARCHAR(500), version INTEGER)");
+
+        //update(connection, "DROP TABLE Playlists IF EXISTS CASCADE");
+        update(connection, "CREATE TABLE Playlists (playlistId INTEGER IDENTITY, name VARCHAR(500))");
+        update(connection, "CREATE INDEX idx_Playlists_name ON Playlists (name)");
+
+        //update(connection, "DROP TABLE PlaylistItems IF EXISTS CASCADE");
+        update(connection,
+                "CREATE TABLE PlaylistItems (playlistItemId INTEGER IDENTITY, filePath VARCHAR(10000), fileName VARCHAR(500), fileSize BIGINT, fileExtension VARCHAR(10), trackTitle VARCHAR(500), time BIGINT, artistName VARCHAR(500), albumName VARCHAR(500), coverArtPath VARCHAR(10000))");
+        update(connection, "CREATE INDEX idx_PlaylistItems_fileName ON PlaylistItems (fileName)");
+        update(connection, "CREATE INDEX idx_PlaylistItems_fileExtension ON PlaylistItems (fileExtension)");
+        update(connection, "CREATE INDEX idx_PlaylistItems_trackTitle ON PlaylistItems (trackTitle)");
+        update(connection, "CREATE INDEX idx_PlaylistItems_artistName ON PlaylistItems (artistName)");
+        update(connection, "CREATE INDEX idx_PlaylistItems_albumName ON PlaylistItems (albumName)");
+
+        //update(connection, "DROP TABLE PlaylistsPlaylistItems IF EXISTS CASCADE");
+        update(connection, "CREATE TABLE PlaylistsPlaylistItems (playlistPlaylistItemId INTEGER IDENTITY, playlistId INTEGER, playlistItemId INTEGER)");
+
+        // INITIAL DATA
+        update(connection, "INSERT INTO LibraryInfo (name , version) VALUES ('" + name + "', " + LIBRARY_DATABASE_VERSION + ")");
 
         return connection;
     }
 
-    private Connection openOrCreateDatabase() {
-        Connection connection = openConnection(false);
+    private Connection openOrCreateDatabase(File path, String name) {
+        Connection connection = openConnection(path, name, false);
         if (connection == null) {
-            return createDatabase();
+            return createDatabase(path, name);
         } else {
             return null;
         }
@@ -110,5 +148,29 @@ public class LibraryDB {
             result.add(row);
         }
         return result;
+    }
+
+    public boolean update(Connection connection, String expression) {
+
+        Statement statment = null;
+
+        try {
+            statment = connection.createStatement();
+
+            int i = statment.executeUpdate(expression);
+
+            return i != -1;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (statment != null) {
+                try {
+                    statment.close();
+                } catch (SQLException e) {
+                }
+            }
+        }
+
+        return false;
     }
 }
