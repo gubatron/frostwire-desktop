8d7bb0d06cbd19878da5b5417b3e73055a8414d4
aldenml <aldenml@gmail.com>
2011-07-16 20:22:35 -0400

    Adding file.

---
+++ components/alexandria/src/main/java/com/frostwire/alexandria/db/LibraryDB.java
@@ -0,0 +1,10 @@
+package com.frostwire.alexandria.db;
+
+public class LibraryDB {
+    
+    private final LibraryDatabase _db;
+
+    public LibraryDB(LibraryDatabase db) {
+        _db = db;
+    }
+}
