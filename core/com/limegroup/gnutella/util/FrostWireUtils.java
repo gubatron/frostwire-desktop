7818c20c5a87fa5fb788f1232aa0a66934d6c438
gubatron <gubatron@gmail.com>
2011-08-10 21:09:28 -0400

    version bump to 5.0.9 for beta test build

--- core/com/limegroup/gnutella/util/FrostWireUtils.java
+++ core/com/limegroup/gnutella/util/FrostWireUtils.java
@@ -25,7 +25,7 @@ public final class FrostWireUtils {
 	/** 
 	 * Constant for the current version of FrostWire.
 	 */
-	private static final String FROSTWIRE_VERSION = "5.0.8";
+	private static final String FROSTWIRE_VERSION = "5.0.9";
     
     /** True if this is a beta. */
     private static final boolean betaVersion = true;
