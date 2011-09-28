fee4e6ea754d0c47cc83b6d3b4699faa2584561e
aldenml <aldenml@gmail.com>
2011-09-28 11:54:19 -0400

    Fixed sort by track number in library.

--- gui/com/frostwire/gui/library/PlaylistItemProperty.java
+++ gui/com/frostwire/gui/library/PlaylistItemProperty.java
@@ -40,22 +40,43 @@ class PlaylistItemProperty implements Comparable<PlaylistItemProperty> {
     	//bitrates come in strings, we only care about the numerical portion
     	//since all of them are in KBPS.
     	if (columnIndex == LibraryPlaylistsTableDataLine.BITRATE_IDX) {
-    		
-    		if (StringUtils.isNullOrEmpty(value) && !StringUtils.isNullOrEmpty(o.value)) {
-    			return 1;
-    		} else if (!StringUtils.isNullOrEmpty(value) && StringUtils.isNullOrEmpty(o.value)) {
-    			return -1;
-    		} else if (StringUtils.isNullOrEmpty(value) && StringUtils.isNullOrEmpty(o.value)) {
-    			return 0;
-    		}
-    		
-    		try {
-    			return Integer.valueOf(value.toLowerCase().replace("kbps", "").trim()).compareTo(Integer.valueOf(o.value.toLowerCase().replace("kbps", "").trim()));
-    		} catch (Exception e) {
-    			return 0;
-    		}
+    		return compareByBitrate(o);
+    	} else if (columnIndex == LibraryPlaylistsTableDataLine.TRACK_IDX) {
+    	    return compareByTrack(o);
     	}
     	
         return value.compareTo(o.value);
+    }
+
+    private int compareByBitrate(PlaylistItemProperty o) {
+        if (StringUtils.isNullOrEmpty(value) && !StringUtils.isNullOrEmpty(o.value)) {
+        	return 1;
+        } else if (!StringUtils.isNullOrEmpty(value) && StringUtils.isNullOrEmpty(o.value)) {
+        	return -1;
+        } else if (StringUtils.isNullOrEmpty(value) && StringUtils.isNullOrEmpty(o.value)) {
+        	return 0;
+        }
+        
+        try {
+        	return Integer.valueOf(value.toLowerCase().replace("kbps", "").trim()).compareTo(Integer.valueOf(o.value.toLowerCase().replace("kbps", "").trim()));
+        } catch (Exception e) {
+        	return 0;
+        }
+    }
+    
+    private int compareByTrack(PlaylistItemProperty o) {
+        if (StringUtils.isNullOrEmpty(value) && !StringUtils.isNullOrEmpty(o.value)) {
+            return 1;
+        } else if (!StringUtils.isNullOrEmpty(value) && StringUtils.isNullOrEmpty(o.value)) {
+            return -1;
+        } else if (StringUtils.isNullOrEmpty(value) && StringUtils.isNullOrEmpty(o.value)) {
+            return 0;
+        }
+        
+        try {
+            return Integer.valueOf(value.toLowerCase().trim().replaceFirst("^0+(?!$)", "")).compareTo(Integer.valueOf(o.value.toLowerCase().trim().replaceFirst("^0+(?!$)", "")));
+        } catch (Exception e) {
+            return 0;
+        }
     }
 }
