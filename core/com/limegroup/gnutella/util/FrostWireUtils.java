ee2cdc145cbfac666435163ab9272cfc5e48927e
gubatron <gubatron@gmail.com>
2011-11-06 12:18:15 -0500

    version bump 5.2.7

--- core/com/limegroup/gnutella/util/FrostWireUtils.java
+++ core/com/limegroup/gnutella/util/FrostWireUtils.java
@@ -30,7 +30,7 @@ public final class FrostWireUtils {
 	/** 
 	 * Constant for the current version of FrostWire.
 	 */
-	private static final String FROSTWIRE_VERSION = "5.2.6";
+	private static final String FROSTWIRE_VERSION = "5.2.7";
     
     /** True if this is a beta. */
     private static final boolean betaVersion = true;
