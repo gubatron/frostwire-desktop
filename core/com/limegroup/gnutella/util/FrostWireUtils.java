16335cc6dc2ef6f682ec580bb78f067e1041adbb
gubatron <gubatron@gmail.com>
2011-11-21 18:22:23 -0500

    5.2.10 version bump, revision to bug reporting system.

--- core/com/limegroup/gnutella/util/FrostWireUtils.java
+++ core/com/limegroup/gnutella/util/FrostWireUtils.java
@@ -30,7 +30,7 @@ public final class FrostWireUtils {
 	/** 
 	 * Constant for the current version of FrostWire.
 	 */
-	private static final String FROSTWIRE_VERSION = "5.2.9";
+	private static final String FROSTWIRE_VERSION = "5.2.10";
 
     /**
      * The cached value of the major revision number.
