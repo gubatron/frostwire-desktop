a5c7c17c9301dc0776da7bb92ba3ac61edb6a7f6
gubatron <gubatron@gmail.com>
2012-02-29 03:14:40 -0500

    Updated help menu entries.

--- gui/com/limegroup/gnutella/gui/menu/HelpMenu.java
+++ gui/com/limegroup/gnutella/gui/menu/HelpMenu.java
@@ -29,21 +29,31 @@ final class HelpMenu extends AbstractMenu {
         super(I18n.tr("Sharing &Help"));
 
         addMenuItem(new OpenLinkAction("http://www.frostwire.com/?id=faq", I18n.tr("&FAQ"), I18n.tr("Frequently Asked Questions for FrostWire")));
+        
+        addMenuItem(new OpenLinkAction("http://www.frostwire.com/android", I18n.tr("FrostWire for Android"),I18n.tr("Get FrostWire on your Android phone, tablet or google tv, all free.")));
 
         addMenuItem(new OpenLinkAction("http://www.frostwire.com/?id=faq#fil7", I18n.tr("What is \"Seeding\"?"), I18n.tr("Learn about BitTorrent Seeding")));
 
         addSeparator();
         
-        addMenuItem(new OpenLinkAction("http://www.frostwire.com/forum/viewforum.php?f=1", I18n.tr("Foru&m"), I18n.tr("Access the FrostWire Users\' Forum")));
-
+        addMenuItem(new OpenLinkAction("http://www.frostwire.com/stickers",I18n.tr("Free FrostWire Stickers"),I18n.tr("Order free FrostWire stickers, we ship worldwide. Stickers are free, you pay cents for shipping.")));
+        
+        addMenuItem(new OpenLinkAction("http://www.frostwire.com/shop",I18n.tr("FrostWire T-Shirts"),I18n.tr("Get a T-shirt, support the project. You'll get also 10 free stickers")));
+        
         addMenuItem(new OpenLinkAction("http://www.frostclick.com/wp/?from=frostwire" + com.limegroup.gnutella.util.FrostWireUtils.getFrostWireVersion(),
                 I18n.tr("FrostClick.com"), I18n.tr("Free Legal Downloads")));
 
-        addMenuItem(new OpenLinkAction("http://www.frostwire.com/twitter", I18n.tr("Follow us on Twitter"), I18n.tr("Follow us on Twitter")));
+        addSeparator();
+
+        addMenuItem(new OpenLinkAction("http://www.facebook.com/pages/FrostWire/110265295669948",I18n.tr("FrostWire on Facebook"),I18n.tr("Come and say hi to the community on Facebook")));
+        
+        addMenuItem(new OpenLinkAction("http://twitter.com/#!/frostwire", I18n.tr("Follow us on Twitter"), I18n.tr("Follow us on Twitter")));
+        
+        addMenuItem(new OpenLinkAction("http://www.frostwire.com/forum/viewforum.php?f=1", I18n.tr("Foru&m"), I18n.tr("Access the FrostWire Users\' Forum")));
 
         addSeparator();
         
-        addMenuItem(new OpenLinkAction("http://frostwire.wordpress.com/2007/03/19/dont-get-scammed-frostwire-is-free/", I18n.tr("&Did you pay for FrostWire?"),
+        addMenuItem(new OpenLinkAction("http://frostwire.wordpress.com/2012/02/14/dont-get-scammed-frostwire-is-free/", I18n.tr("&Did you pay for FrostWire?"),
                 I18n.tr("Did you pay for FrostWire? FrostWire is Free as in Free Beer. Avoid Scams.")));
 
         addMenuItem(new ShowTipOfTheDayAction());
