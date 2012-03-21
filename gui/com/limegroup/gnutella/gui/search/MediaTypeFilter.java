d70851b306520b1fcbeca9a7527eda944b63db26
aldenml <aldenml@gmail.com>
2012-03-21 17:21:37 -0400

    Fixed filter for youtube extension.

--- gui/com/limegroup/gnutella/gui/search/MediaTypeFilter.java
+++ gui/com/limegroup/gnutella/gui/search/MediaTypeFilter.java
@@ -5,37 +5,46 @@ import com.limegroup.gnutella.MediaType;
 import com.limegroup.gnutella.settings.SearchSettings;
 
 public class MediaTypeFilter implements TableLineFilter<SearchResultDataLine> {
-    
+
     private final boolean allowAll;
-    
+
     public MediaTypeFilter() {
         this.allowAll = isAllSelected();
     }
 
     @Override
     public boolean allow(SearchResultDataLine node) {
+        try {
+            // hard coding disable youtube extension
+            if (node.getExtension().equals("youtube")) {
+                return false;
+            }
+        } catch (Throwable e) {
+            // ignore
+        }
+
         if (allowAll) {
             return true;
         }
-        
+
         NamedMediaType nmt = node.getNamedMediaType();
         if (nmt != null) {
             return SearchSettings.LAST_MEDIA_TYPES_USED.contains(nmt.getMediaType().getMimeType());
         }
-        
+
         return false;
     }
-    
+
     private boolean isAllSelected() {
         boolean selected = true;
-        
+
         selected &= SearchSettings.LAST_MEDIA_TYPES_USED.contains(MediaType.getAudioMediaType().getMimeType());
         selected &= SearchSettings.LAST_MEDIA_TYPES_USED.contains(MediaType.getVideoMediaType().getMimeType());
         selected &= SearchSettings.LAST_MEDIA_TYPES_USED.contains(MediaType.getImageMediaType().getMimeType());
         selected &= SearchSettings.LAST_MEDIA_TYPES_USED.contains(MediaType.getDocumentMediaType().getMimeType());
         selected &= SearchSettings.LAST_MEDIA_TYPES_USED.contains(MediaType.getProgramMediaType().getMimeType());
         selected &= SearchSettings.LAST_MEDIA_TYPES_USED.contains(MediaType.getTorrentMediaType().getMimeType());
-        
+
         return selected;
     }
 }
