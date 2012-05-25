540a0de929b40fec6787c64aad77adbc8f053b91
aldenml <aldenml@gmail.com>
2012-05-25 01:46:51 -0400

    Fix for skin sub menu separators in popups.

--- gui/com/limegroup/gnutella/gui/themes/SkinMenu.java
+++ gui/com/limegroup/gnutella/gui/themes/SkinMenu.java
@@ -1,18 +1,38 @@
 package com.limegroup.gnutella.gui.themes;
 
+import java.lang.reflect.Field;
+
 import javax.swing.JMenu;
 
 public class SkinMenu extends JMenu {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 8803396907396504057L;
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 8803396907396504057L;
 
-	public SkinMenu() {
-	}
-	
-	public SkinMenu(String s) {
-		super(s);
-	}
+    public SkinMenu() {
+        ensurePopupMenuCreated();
+    }
+
+    public SkinMenu(String s) {
+        super(s);
+
+        ensurePopupMenuCreated();
+    }
+
+    private void ensurePopupMenuCreated() {
+        SkinPopupMenu popupMenu = new SkinPopupMenu();
+        popupMenu.setInvoker(this);
+        popupListener = createWinListener(popupMenu);
+        
+        try {
+            Field f = JMenu.class.getDeclaredField("popupMenu");
+            f.setAccessible(true);
+            f.set(this, popupMenu);
+        } catch (Throwable e) {
+            e.printStackTrace();
+            // ignore
+        }
+    }
 }
