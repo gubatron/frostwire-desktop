db05a916492600fae93651893eaf30afa24bd241
aldenml <aldenml@gmail.com>
2012-05-24 22:45:50 -0400

    Formatting and general revision of iTunesMediator.

--- gui/com/limegroup/gnutella/gui/iTunesMediator.java
+++ gui/com/limegroup/gnutella/gui/iTunesMediator.java
@@ -1,9 +1,29 @@
+/*
+ * Created by Angel Leon (@gubatron), Alden Torres (aldenml)
+ * Copyright (c) 2011, 2012, FrostWire(TM). All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.limegroup.gnutella.gui;
 
 import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -13,7 +33,6 @@ import java.util.concurrent.ExecutorService;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.gudy.azureus2.core3.util.FileUtil;
 import org.limewire.concurrent.ExecutorsHelper;
 import org.limewire.util.CommonUtils;
 import org.limewire.util.FileUtils;
@@ -31,9 +50,9 @@ import com.limegroup.gnutella.settings.iTunesSettings;
 public final class iTunesMediator {
 
     private static final Log LOG = LogFactory.getLog(iTunesMediator.class);
-    
+
     private static final String JS_IMPORT_SCRIPT_NAME = "itunes_import.js";
-    
+
     private static final String JS_REMOVE_PLAYLIST_SCRIPT_NAME = "itunes_remove_playlist.js";
 
     private static iTunesMediator INSTANCE;
@@ -42,7 +61,7 @@ public final class iTunesMediator {
      * The queue that will process the tunes to add.
      */
     private final ExecutorService QUEUE = ExecutorsHelper.newProcessingQueue("iTunesAdderThread");
-    
+
     /**
      * Returns the sole instance of this class.
      */
@@ -57,6 +76,9 @@ public final class iTunesMediator {
      * Initializes iTunesMediator with the script file.
      */
     private iTunesMediator() {
+        if (OSUtils.isWindows()) {
+            createiTunesJavaScript(JS_IMPORT_SCRIPT_NAME);
+        }
     }
 
     /**
@@ -64,54 +86,54 @@ public final class iTunesMediator {
      * is a supported type, send it to iTunes.
      */
     private void addSongsITunes(File file) {
-        
+
         // Make sure we convert any uppercase to lowercase or vice versa.
         try {
             file = FileUtils.getCanonicalFile(file);
-        } catch (IOException ignored) {}
-        
+        } catch (IOException ignored) {
+        }
+
         // Verify that we're adding a real file.
         if (!file.exists()) {
-            if (LOG.isDebugEnabled())
-                LOG.debug("File: '" + file + "' does not exist");
+            LOG.warn("File: '" + file + "' does not exist");
             return;
         }
-        
-        
-        
-        File [] files;
+
+        File[] files;
         if (file.isDirectory()) {
-        	files = FileUtils.getFilesRecursive(file, 
-        			iTunesSettings.ITUNES_SUPPORTED_FILE_TYPES.getValue());
-        } else if (file.isFile() && isSupported(FileUtils.getFileExtension(file)))
-        	files = new File[]{file};
-        else
-        	return;
-        
-        if (files.length == 0) {
-        	return;
+            files = FileUtils.getFilesRecursive(file, iTunesSettings.ITUNES_SUPPORTED_FILE_TYPES.getValue());
+        } else if (file.isFile() && isSupported(FileUtils.getFileExtension(file))) {
+            files = new File[] { file };
+        } else {
+            return;
         }
-        
+
+        if (files.length == 0) {
+            return;
+        }
+
         //remove incomplete files from files.
         Set<File> incompleteFiles = TorrentUtil.getIncompleteFiles();
         incompleteFiles.addAll(TorrentUtil.getSkipedFiles());
-        
+
         List<File> completeFiles = new ArrayList<File>(files.length);
         for (File f : files) {
-        	if (incompleteFiles.contains(f))
-        		continue;
-        	
-        	completeFiles.add(f);
+            if (incompleteFiles.contains(f)) {
+                continue;
+            }
+
+            completeFiles.add(f);
         }
-        
+
         files = completeFiles.toArray(new File[0]);
-        
+
         if (OSUtils.isMacOSX()) {
-        	QUEUE.execute(new ExecOSAScriptCommand(files));
+            QUEUE.execute(new ExecOSAScriptCommand(files));
         } else {
-        	if (LOG.isTraceEnabled())
+            if (LOG.isTraceEnabled()) {
                 LOG.trace("Will add '" + files.length + " files" + "' to Playlist");
-        	QUEUE.execute(new ExecWSHScriptCommand(files));
+            }
+            QUEUE.execute(new ExecWSHScriptCommand(files));
         }
     }
 
@@ -119,63 +141,54 @@ public final class iTunesMediator {
      * Returns true if the extension of name is a supported file type.
      */
     private static boolean isSupported(String extension) {
-    	if (extension == null)
-    		return false;
+        if (extension == null) {
+            return false;
+        }
+
         String[] types = iTunesSettings.ITUNES_SUPPORTED_FILE_TYPES.getValue();
-        for (int i = 0; i < types.length; i++)
-            if (extension.equalsIgnoreCase(types[i]))
+        for (int i = 0; i < types.length; i++) {
+            if (extension.equalsIgnoreCase(types[i])) {
                 return true;
+            }
+        }
         return false;
     }
-    
+
     /**
      * Constructs and returns a osascript command.
      */
     private static String[] createOSAScriptCommand(File[] files) {
-        
+
         String playlist = iTunesSettings.ITUNES_PLAYLIST.getValue();
-        
+
         List<String> command = new ArrayList<String>();
-        command.addAll(Arrays.asList( 
-            "osascript", 
-            "-e", "tell application \"Finder\""));
+        command.addAll(Arrays.asList("osascript", "-e", "tell application \"Finder\""));
 
-        for (int i=0; i < files.length; i++) {
-        	File f = files[i];
-        	String path = f.getAbsolutePath();
-        	command.add( "-e");
-        	command.add("set hfsFile"+i+" to (POSIX file \"" + path + "\")" );
-        }
-        
-
-        command.addAll(Arrays.asList(
-            "-e",   "set thePlaylist to \"" + playlist + "\"", 
-            "-e",   "tell application \"iTunes\"",
-            //"-e",       "activate", // launch and bring to front
-            "-e",       "launch", // launch in background
-            "-e",       "if not (exists playlist thePlaylist) then", 
-            "-e",           "set thisPlaylist to make new playlist", 
-            "-e",           "set name of thisPlaylist to thePlaylist", 
-            "-e",       "end if"
-            ));
-
-        for (int i=0; i < files.length; i++) {
-        	command.addAll(Arrays.asList("-e",       "add hfsFile"+i+" to playlist thePlaylist"));
+        for (int i = 0; i < files.length; i++) {
+            File f = files[i];
+            String path = f.getAbsolutePath();
+            command.add("-e");
+            command.add("set hfsFile" + i + " to (POSIX file \"" + path + "\")");
         }
 
-        command.addAll(Arrays.asList(
-        "-e",   "end tell",
-        "-e", "end tell")); 
+        command.addAll(Arrays.asList("-e", "set thePlaylist to \"" + playlist + "\"", "-e", "tell application \"iTunes\"",
+        //"-e",       "activate", // launch and bring to front
+                "-e", "launch", // launch in background
+                "-e", "if not (exists playlist thePlaylist) then", "-e", "set thisPlaylist to make new playlist", "-e", "set name of thisPlaylist to thePlaylist", "-e", "end if"));
+
+        for (int i = 0; i < files.length; i++) {
+            command.addAll(Arrays.asList("-e", "add hfsFile" + i + " to playlist thePlaylist"));
+        }
+
+        command.addAll(Arrays.asList("-e", "end tell", "-e", "end tell"));
 
         return command.toArray(new String[0]);
     }
-    
+
     private static String[] createWSHScriptCommand(File[] files) {
-    	
-    	createJScriptIfNeeded(JS_IMPORT_SCRIPT_NAME);
-    	
+
         String playlist = iTunesSettings.ITUNES_PLAYLIST.getValue();
-        
+
         ArrayList<String> command = new ArrayList<String>();
         command.add("wscript");
         command.add("//B");
@@ -183,12 +196,12 @@ public final class iTunesMediator {
         command.add(new File(CommonUtils.getUserSettingsDir(), JS_IMPORT_SCRIPT_NAME).getAbsolutePath());
         command.add(playlist);
         for (File file : files) {
-        	command.add(file.getAbsolutePath());
+            command.add(file.getAbsolutePath());
         }
-        
+
         return command.toArray(new String[0]);
     }
-    
+
     /**
      * Executes the osascript CLI command
      */
@@ -211,12 +224,12 @@ public final class iTunesMediator {
         public void run() {
             try {
                 Runtime.getRuntime().exec(createOSAScriptCommand(files));
-            } catch (IOException err) {
-                LOG.debug(err);
+            } catch (IOException e) {
+                LOG.debug(e);
             }
         }
     }
-    
+
     private class ExecWSHScriptCommand implements Runnable {
         /**
          * The file to add.
@@ -236,93 +249,88 @@ public final class iTunesMediator {
         public void run() {
             try {
                 Runtime.getRuntime().exec(createWSHScriptCommand(files));
-            } catch (IOException err) {
-                LOG.debug(err);
+            } catch (IOException e) {
+                LOG.debug(e);
             }
         }
     }
 
-	public void scanForSongs(File file) {
-	    iTunesImportSettings.IMPORT_FILES.add(file);
-	    if (OSUtils.isMacOSX() || OSUtils.isWindows()) {
-			addSongsITunes(file);
-		} else if (OSUtils.isUbuntu()) {
-		    //System.out.println("Import in Banshee: " + file);
-		}
-	}
+    public void scanForSongs(File file) {
+        iTunesImportSettings.IMPORT_FILES.add(file);
+        if (OSUtils.isMacOSX() || OSUtils.isWindows()) {
+            addSongsITunes(file);
+        } else if (OSUtils.isUbuntu()) {
+            //System.out.println("Import in Banshee: " + file);
+        }
+    }
 
     public boolean isScanned(File file) {
         return iTunesImportSettings.IMPORT_FILES.contains(file);
     }
-    
+
     public void removeFromScanned(File file) {
         iTunesImportSettings.IMPORT_FILES.remove(file);
     }
-    
-    private static void createJScriptIfNeeded(String scriptName) {
-    	File fileJS = new File(CommonUtils.getUserSettingsDir(), scriptName);
-    	if (fileJS.exists()) {
-    		return;
-    	}
-    	
+
+    private static void createiTunesJavaScript(String scriptName) {
+        File fileJS = new File(CommonUtils.getUserSettingsDir(), scriptName);
+        if (fileJS.exists()) {
+            return;
+        }
+
         URL url = ResourceManager.getURLResource(scriptName);
+
         InputStream is = null;
+        OutputStream out = null;
+
         try {
-            if(url != null) {
+            if (url != null) {
                 is = new BufferedInputStream(url.openStream());
-                FileUtil.copyFile(is, fileJS, false);
+                out = new FileOutputStream(fileJS);
+                IOUtils.copy(is, out);
             }
-        } catch(IOException ignored) {
+        } catch (IOException e) {
+            LOG.error("Error creating iTunes javascript", e);
         } finally {
-            IOUtils.close(is);
+            IOUtils.closeQuietly(is);
+            IOUtils.closeQuietly(out);
         }
     }
 
     public void deleteFrostWirePlaylist() {
-    	String playlistName = iTunesSettings.ITUNES_PLAYLIST.getValue();
+        String playlistName = iTunesSettings.ITUNES_PLAYLIST.getValue();
 
-    	if (OSUtils.isMacOSX()) {
-            String[] command = new String[] { 
-                    "osascript", 
-                    "-e", "tell application \"iTunes\"", 
-                    "-e",   "delete playlist \"" + playlistName + "\"", 
-                    "-e", "end tell" 
-                };
-            try {
-				Runtime.getRuntime().exec(command);
-			} catch (IOException e) {
-			}
-    	} else if (OSUtils.isWindows()) {
-    		createJScriptIfNeeded(JS_REMOVE_PLAYLIST_SCRIPT_NAME);
-    		
-            ArrayList<String> command = new ArrayList<String>();
-            command.add("wscript");
-            command.add("//B");
-            command.add("//NoLogo");
-            command.add(new File(CommonUtils.getUserSettingsDir(), JS_REMOVE_PLAYLIST_SCRIPT_NAME).getAbsolutePath());
+        try {
+            if (OSUtils.isMacOSX()) {
+                String[] command = new String[] { "osascript", "-e", "tell application \"iTunes\"", "-e", "delete playlist \"" + playlistName + "\"", "-e", "end tell" };
 
-            try {
-				Runtime.getRuntime().exec(command.toArray(new String[0]));
-			} catch (IOException e) {
-				e.printStackTrace();
-			}            
-    	}
+                Runtime.getRuntime().exec(command);
+            } else if (OSUtils.isWindows()) {
+                ArrayList<String> command = new ArrayList<String>();
+                command.add("wscript");
+                command.add("//B");
+                command.add("//NoLogo");
+                command.add(new File(CommonUtils.getUserSettingsDir(), JS_REMOVE_PLAYLIST_SCRIPT_NAME).getAbsolutePath());
+
+                Runtime.getRuntime().exec(command.toArray(new String[0]));
+            }
+        } catch (IOException e) {
+            LOG.error("Error executing itunes command", e);
+        }
     }
-    
-	public void resetFrostWirePlaylist() {
-		deleteFrostWirePlaylist();
-		
-		QUEUE.execute(new Runnable() {
-			@Override
-			public void run() {
-				try {
-					Thread.sleep(1000);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-				iTunesMediator.instance().scanForSongs(SharingSettings.TORRENT_DATA_DIR_SETTING.getValue());			
-			}
-		});
-	}
-	
+
+    public void resetFrostWirePlaylist() {
+        deleteFrostWirePlaylist();
+
+        QUEUE.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                }
+                iTunesMediator.instance().scanForSongs(SharingSettings.TORRENT_DATA_DIR_SETTING.getValue());
+            }
+        });
+    }
 }
