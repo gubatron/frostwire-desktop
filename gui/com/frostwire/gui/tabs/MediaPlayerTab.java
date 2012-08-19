6a7ea69cb07672e075266cf8810477ec0948879a
Alden Torres <aldenml@gmail.com>
2012-08-18 21:54:07 -0400

    Missing headers.

--- gui/com/frostwire/gui/tabs/MediaPlayerTab.java
+++ gui/com/frostwire/gui/tabs/MediaPlayerTab.java
@@ -1,3 +1,21 @@
+/*
+ * Created by Angel Leon (@gubatron), Alden Torres (aldenml)
+ * Copyright (c) 2011, 2012, FrostWire(R). All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.frostwire.gui.tabs;
 
 import java.awt.BorderLayout;
@@ -9,6 +27,12 @@ import com.frostwire.gui.mplayer.MPlayerComponent;
 import com.frostwire.gui.mplayer.MPlayerComponentFactory;
 import com.limegroup.gnutella.gui.I18n;
 
+/**
+ * 
+ * @author gubatron
+ * @author aldenml
+ *
+ */
 public final class MediaPlayerTab extends AbstractTab {
 
     private static JPanel PANEL = new JPanel(new BorderLayout());
