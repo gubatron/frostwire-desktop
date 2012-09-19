1c5145776c9064d82de33ea7adb7a2c019274e33
erichpleny <erich.pleny@gmail.com>
2012-09-19 14:24:07 -0400

    updating JNI project settings, MacOSXUtils code for autorun, andcompiled library

--- components/common/src/main/native/osx/MacOSXUtils.m
+++ components/common/src/main/native/osx/MacOSXUtils.m
@@ -13,7 +13,7 @@
 extern "C" {
 #endif
 
-#define OS_NATIVE(func) Java_org_limewire_ui_swing_util_MacOSXUtils_##func
+#define OS_NATIVE(func) Java_com_limegroup_gnutella_util_MacOSXUtils_##func
 
 JNIEXPORT jstring JNICALL OS_NATIVE(GetCurrentFullUserName)
 	(JNIEnv *env, jobject clazz)
@@ -27,41 +27,58 @@ JNIEXPORT jstring JNICALL OS_NATIVE(GetCurrentFullUserName)
 }
 
 JNIEXPORT void JNICALL OS_NATIVE(SetLoginStatusNative)
-	(JNIEnv *env, jobject obj, jboolean onoff)
+	(JNIEnv *env, jobject obj, jboolean onoff, jstring path )
 {
-	NSMutableArray *loginItems;
-	NSDictionary *appDict;
-	NSEnumerator *appEnum;
-	NSString *agentAppPath = @"/Applications/LimeWire/LimeWire.app";
-	
-	// Make a mutable copy (why a copy?)
-	loginItems = (NSMutableArray *)CFPreferencesCopyValue((CFStringRef)@"AutoLaunchedApplicationDictionary", 
-																												(CFStringRef)@"loginwindow", kCFPreferencesCurrentUser, 
-																												kCFPreferencesAnyHost);
-	loginItems = [[loginItems autorelease] mutableCopy];
-	appEnum = [loginItems objectEnumerator];
-	
-	while ((appDict = [appEnum nextObject])) {
-		if ([[[appDict objectForKey:@"Path"] stringByExpandingTildeInPath] isEqualToString:agentAppPath])
-			break;
-	}
-	
-	// register the item
-	if (onoff == JNI_TRUE) {
-		if (!appDict)
-			[loginItems addObject:[NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithBool:NO], @"Hide", agentAppPath, @"Path", nil]];
-	}
-	// unregister the item
-	else if (appDict)
-		[loginItems removeObject:appDict];
-	
-	CFPreferencesSetValue((CFStringRef)@"AutoLaunchedApplicationDictionary", 
-												loginItems, (CFStringRef)@"loginwindow", 
-												kCFPreferencesCurrentUser, kCFPreferencesAnyHost);
-	
-	CFPreferencesSynchronize((CFStringRef)@"loginwindow", kCFPreferencesCurrentUser, kCFPreferencesAnyHost);
-	
-	[loginItems release];
+    // convert the path to a local NSString
+    const jchar *chars = (*env)->GetStringChars(env, path, NULL);
+    NSString *appPath = [NSString stringWithCharacters:(UniChar *)chars
+                                                   length:(*env)->GetStringLength(env, path)];
+    (*env)->ReleaseStringChars(env, path, chars);
+    
+	// Create url of path
+	CFURLRef url = (CFURLRef)[NSURL fileURLWithPath:appPath];
+    
+	// Create a reference to the shared file list for current user only.  all users use: kLSSharedFileListGlobalLoginItems
+	LSSharedFileListRef loginItems = LSSharedFileListCreate(NULL,
+                                                            kLSSharedFileListSessionLoginItems, NULL);
+	if (loginItems) {
+        
+        if (onoff) {
+        
+            //Insert an item to the list.
+            LSSharedFileListItemRef item = LSSharedFileListInsertItemURL(loginItems,
+                                                                     kLSSharedFileListItemLast, NULL, NULL,
+                                                                     url, NULL, NULL);
+            if (item){
+                CFRelease(item);
+            }
+            
+        } else {
+            
+            //Remove item from the list
+            UInt32 seedValue;
+            
+            //Retrieve the list of Login Items
+            NSArray  *loginItemsArray = (NSArray *)LSSharedFileListCopySnapshot(loginItems, &seedValue);
+            
+            for(int i=0; i< [loginItemsArray count]; i++){
+                LSSharedFileListItemRef itemRef = (LSSharedFileListItemRef)[loginItemsArray
+                                                                        objectAtIndex:i];
+                //Resolve the item with URL
+                if (LSSharedFileListItemResolve(itemRef, 0, (CFURLRef*) &url, NULL) == noErr) {
+                    NSString * urlPath = [(NSURL*)url path];
+                    
+                    if ([urlPath compare:appPath] == NSOrderedSame){
+                        LSSharedFileListItemRemove(loginItems,itemRef);
+                    }
+                }
+            }
+            
+            [loginItemsArray release];
+        }
+    
+        CFRelease(loginItems);
+    }
 }
 
 #ifdef __cplusplus
