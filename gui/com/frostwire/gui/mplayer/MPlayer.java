b40dc16b47f834cca470ae52f896ba1cf9fb2938
erichpleny <erich.pleny@gmail.com>
2012-11-19 17:43:01 +0100

    removing unused MPlayerApp.  adding GPL headers to source files

--- gui/com/frostwire/gui/mplayer/MPlayer.java
+++ gui/com/frostwire/gui/mplayer/MPlayer.java
@@ -1,4 +1,7 @@
 /*
+ * Created by Angel Leon (@gubatron), Alden Torres (aldenml), Erich Pleny (erichpleny)
+ * Copyright (c) 2012, FrostWire(R). All rights reserved.
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
