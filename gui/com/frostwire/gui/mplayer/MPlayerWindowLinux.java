481666850a89739c778e03970d28f2ded5d82238
erichpleny <erich.pleny@gmail.com>
2012-12-03 12:23:41 +0100

    mplayer-linux: removing dead code, unused imports, and cleaning up toggle boolean logic.

--- gui/com/frostwire/gui/mplayer/MPlayerWindowLinux.java
+++ gui/com/frostwire/gui/mplayer/MPlayerWindowLinux.java
@@ -1,18 +1,11 @@
 package com.frostwire.gui.mplayer;
 
-import org.limewire.util.SystemUtils;
-
 import sun.awt.X11.XComponentPeer;
-import sun.awt.X11.XWindow;
 
 public class MPlayerWindowLinux extends MPlayerWindow {
 
 	private static final long serialVersionUID = -4373778544356324171L;
 	private static boolean isFullScreen = false;
-	
-	public MPlayerWindowLinux() {
-		System.out.println("MPlayerWindowLinux hwnd: " + getHwnd());
-	}
 	
 	@Override
 	public long getCanvasComponentHwnd() {
@@ -38,12 +31,10 @@ public class MPlayerWindowLinux extends MPlayerWindow {
 	
 	@Override
 	public void toggleFullScreen() {
-		//SystemUtils.toggleFullScreen(getHwnd());
-		
-		setExtendedState( isFullScreen ? NORMAL : MAXIMIZED_BOTH);
-		super.toggleFullScreen();
 		
 		isFullScreen = !isFullScreen;
 		
+		setExtendedState( isFullScreen ? MAXIMIZED_BOTH : NORMAL);
+		super.toggleFullScreen();
 	}
 }
