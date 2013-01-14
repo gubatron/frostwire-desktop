f87209d9340264dd023dcc6c927983155fc436e3
Gubatron <gubatron@gmail.com>
2013-01-14 17:18:12 -0500

    Longer http timeouts for slower connections

--- core/com/frostwire/bittorrent/websearch/WebSearchPerformer.java
+++ core/com/frostwire/bittorrent/websearch/WebSearchPerformer.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public interface WebSearchPerformer {
     
-    public static final int HTTP_TIMEOUT = 5000;
+    public static final int HTTP_TIMEOUT = 10000;
     
     public List<WebSearchResult> search(String keywords);
 }
