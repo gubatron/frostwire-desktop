1bae6ba89f243f9057ec5adbe3dc6cea349c93ae
Alden Torres <aldenml@gmail.com>
2013-01-23 18:40:58 -0500

    Working in new player component for Mac OSX.

--- gui/com/frostwire/gui/mplayer/MPlayerComponentOSX2.java
+++ gui/com/frostwire/gui/mplayer/MPlayerComponentOSX2.java
@@ -20,7 +20,11 @@ package com.frostwire.gui.mplayer;
 
 import java.awt.Canvas;
 import java.awt.Component;
+import java.awt.Dimension;
 
+import javax.swing.SwingUtilities;
+
+import com.frostwire.gui.player.MPlayerUIEventHandler;
 import com.frostwire.gui.player.MediaPlayer;
 import com.frostwire.gui.player.MediaPlayerListener;
 import com.frostwire.gui.player.MediaSource;
@@ -38,42 +42,222 @@ public class MPlayerComponentOSX2 extends Canvas implements MPlayerComponent, Me
         System.loadLibrary("JMPlayer");
     }
 
+    private long view;
+
+    private static final int JMPlayer_addNotify = 1;
+    private static final int JMPlayer_dispose = 2;
+
+    // ui events
+    private static final int JMPlayer_volumeChanged = 3;
+    private static final int JMPlayer_timeInitialized = 4;
+    private static final int JMPlayer_progressChanged = 5;
+    private static final int JMPlayer_stateChanged = 6;
+    private static final int JMPlayer_toggleFS = 7;
+
+    // states
+    private static final int JMPlayer_statePlaying = 1;
+    private static final int JMPlayer_statePaused = 2;
+    private static final int JMPlayer_stateClosed = 3;
+
+    private boolean refreshPlayTime = false;
+
+    public MPlayerComponentOSX2() {
+        MediaPlayer.instance().addMediaPlayerListener(this);
+    }
+
+    @Override
+    public Dimension getMaximumSize() {
+        return new Dimension(Short.MAX_VALUE, Short.MAX_VALUE);
+    }
+
+    @Override
+    public Dimension getMinimumSize() {
+        return new Dimension(25, 25);
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(250, 250);
+    }
+
     @Override
     public void addNotify() {
         super.addNotify();
-        addNSView();
+        view = createNSView();
+        sendMsg(JMPlayer_addNotify);
     }
 
-    public native void addNSView();
+    @Override
+    protected void finalize() throws Throwable {
+        dispose();
+        super.finalize();
+    }
+
+    @Override
+    public boolean toggleFullScreen() {
+        sendMsg(JMPlayer_toggleFS);
+        return true;
+    }
+
+    protected void dispose() {
+        sendMsg(JMPlayer_dispose);
+    }
+
+    /*
+     * JNI Hook methods - forward all to the UIEventHandler
+     */
+    public void onVolumeChanged(final float volume) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                MPlayerUIEventHandler.instance().onVolumeChanged(volume);
+            }
+        });
+    }
+
+    public void onIncrementVolumePressed() {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                MPlayerUIEventHandler.instance().onVolumeIncremented();
+            }
+        });
+    }
+
+    public void onDecrementVolumePressed() {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                MPlayerUIEventHandler.instance().onVolumeDecremented();
+            }
+        });
+    }
+
+    public void onSeekToTime(final float seconds) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                MPlayerUIEventHandler.instance().onSeekToTime(seconds);
+            }
+        });
+    }
+
+    public void onPlayPressed() {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                MPlayerUIEventHandler.instance().onPlayPressed();
+            }
+        });
+    }
+
+    public void onPausePressed() {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                MPlayerUIEventHandler.instance().onPausePressed();
+            }
+        });
+    }
+
+    public void onFastForwardPressed() {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                MPlayerUIEventHandler.instance().onFastForwardPressed();
+            }
+        });
+    }
+
+    public void onRewindPressed() {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                MPlayerUIEventHandler.instance().onRewindPressed();
+            }
+        });
+    }
+
+    public void onToggleFullscreenPressed() {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                MPlayerUIEventHandler.instance().onToggleFullscreenPressed();
+            }
+        });
+    }
+
+    public void onProgressSliderStarted() {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                MPlayerUIEventHandler.instance().onProgressSlideStart();
+            }
+        });
+    }
+
+    public void onProgressSliderEnded() {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                MPlayerUIEventHandler.instance().onProgressSlideEnd();
+            }
+        });
+    }
 
     @Override
     public void mediaOpened(MediaPlayer mediaPlayer, MediaSource mediaSource) {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void progressChange(MediaPlayer mediaPlayer, float currentTimeInSecs) {
-        // TODO Auto-generated method stub
-        
+        sendMsg(JMPlayer_progressChanged, Float.valueOf(currentTimeInSecs));
     }
 
     @Override
     public void volumeChange(MediaPlayer mediaPlayer, double currentVolume) {
-        // TODO Auto-generated method stub
-        
+        sendMsg(JMPlayer_volumeChanged, Float.valueOf((float) currentVolume));
     }
 
     @Override
     public void stateChange(MediaPlayer mediaPlayer, MediaPlaybackState state) {
-        // TODO Auto-generated method stub
-        
+        int s;
+
+        switch (state) {
+        case Playing:
+            s = JMPlayer_statePlaying;
+            break;
+        case Paused:
+            s = JMPlayer_statePaused;
+            break;
+        case Closed:
+            s = JMPlayer_stateClosed;
+            break;
+        default:
+            s = -1;
+            break;
+        }
+
+        if (state == MediaPlaybackState.Playing && refreshPlayTime) {
+            refreshPlayTime = false;
+            sendMsg(JMPlayer_timeInitialized, Float.valueOf(MediaPlayer.instance().getDurationInSecs()));
+        }
+
+        if (state != MediaPlaybackState.Playing) {
+            refreshPlayTime = true;
+        }
+
+        if (s != -1) {
+            sendMsg(JMPlayer_stateChanged, Integer.valueOf(s));
+        }
     }
 
     @Override
     public void icyInfo(MediaPlayer mediaPlayer, String data) {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
@@ -82,14 +266,20 @@ public class MPlayerComponentOSX2 extends Canvas implements MPlayerComponent, Me
     }
 
     @Override
-    public boolean toggleFullScreen() {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-    @Override
     public long getWindowID() {
         // TODO Auto-generated method stub
         return 0;
     }
+
+    private void sendMsg(int messageID) {
+        sendMsg(messageID, null);
+    }
+
+    private void sendMsg(int messageID, Object message) {
+        awtMessage(view, messageID, message);
+    }
+
+    private native long createNSView();
+
+    private native void awtMessage(long view, int messageID, Object message);
 }
