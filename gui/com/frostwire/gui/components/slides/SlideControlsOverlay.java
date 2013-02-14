9a7882f35b3bb32be0f085378b55aa0c408ccfaa
Alden Torres <aldenml@gmail.com>
2013-02-14 10:50:27 -0500

    New images for slide overlay buttons.

--- gui/com/frostwire/gui/components/slides/SlideControlsOverlay.java
+++ gui/com/frostwire/gui/components/slides/SlideControlsOverlay.java
@@ -3,7 +3,6 @@ package com.frostwire.gui.components.slides;
 import java.awt.AlphaComposite;
 import java.awt.Color;
 import java.awt.Composite;
-import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
@@ -18,20 +17,26 @@ import com.limegroup.gnutella.gui.IconButton;
 import com.limegroup.gnutella.gui.actions.AbstractAction;
 import com.limegroup.gnutella.gui.actions.LimeAction;
 
-public class SlideControlsOverlay extends JPanel {
-    private SlidePanelController controller;
+class SlideControlsOverlay extends JPanel {
+
+    private static final Color BACKGROUND = new Color(45, 52, 58); // 2d343a
+    private static final float BACKGROUND_ALPHA = 0.7f;
+
+    private final SlidePanelController controller;
+    private final Composite overlayComposite;
 
     public SlideControlsOverlay(SlidePanelController controller) {
         this.controller = controller;
+        this.overlayComposite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, BACKGROUND_ALPHA);
+
         setupUI();
     }
 
     private void setupUI() {
         setOpaque(false);
-        setLayout(new MigLayout("",
-                                "[grow][][][][grow]", //columns
-                                "[grow][center][grow]")); //rows
-        setBackground(new Color(0, 0, 0));
+        setLayout(new MigLayout("", "[grow][][][][grow]", //columns
+                "[grow][center][grow]")); //rows
+        setBackground(BACKGROUND);
         setupButtons();
     }
 
@@ -41,33 +46,34 @@ public class SlideControlsOverlay extends JPanel {
 
             if (slide.hasFlag(Slide.POST_DOWNLOAD_EXECUTE)) {
                 //add install button
-                add(new IconButton(new InstallAction(controller)),"cell 1 1");// "cell column row width height"
+                add(new IconButton(new InstallAction(controller)), "cell 1 1");// "cell column row width height"
             } else {
                 //add download button
-                add(new IconButton(new DownloadAction(controller)),"cell 1 1");
+                add(new IconButton(new DownloadAction(controller)), "cell 1 1");
             }
         }
 
         if (slide.hasFlag(Slide.SHOW_VIDEO_PREVIEW_BUTTON)) {
             //add video preview button
-            add(new IconButton(new PreviewVideoAction(controller)),"cell 2 1");
+            add(new IconButton(new PreviewVideoAction(controller)), "cell 2 1");
         }
 
         if (slide.hasFlag(Slide.SHOW_AUDIO_PREVIEW_BUTTON)) {
             //add audio preview button
-            add(new IconButton(new PreviewAudioAction(controller)),"cell 3 1");
+            add(new IconButton(new PreviewAudioAction(controller)), "cell 3 1");
         }
     }
 
     @Override
     public void paint(Graphics g) {
-        Color background = getBackground();
         Graphics2D g2d = (Graphics2D) g;
 
+        Color background = getBackground();
+
         Composite c = g2d.getComposite();
-        g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.7f));
-        g.setColor(background);
-        g.fillRect(0, 0, getWidth(), getHeight());
+        g2d.setComposite(overlayComposite);
+        g2d.setColor(background);
+        g2d.fillRect(0, 0, getWidth(), getHeight());
         g2d.setComposite(c);
 
         super.paint(g);
@@ -82,15 +88,15 @@ public class SlideControlsOverlay extends JPanel {
             putValue(Action.NAME, I18n.tr("Install"));
             putValue(LimeAction.SHORT_NAME, I18n.tr("Install"));
             putValue(Action.SHORT_DESCRIPTION, I18n.tr("Install") + " " + controller.getSlide().title);
-            putValue(LimeAction.ICON_NAME, "DOWNLOAD_FILE_MORE_SOURCES");
+            putValue(LimeAction.ICON_NAME, "SLIDE_CONTROLS_OVERLAY_DOWNLOAD");
         }
 
         @Override
-        public void actionPerformed(ActionEvent arg0) {
+        public void actionPerformed(ActionEvent e) {
             controller.installSlide();
         }
     }
-    
+
     private static final class DownloadAction extends AbstractAction {
 
         private SlidePanelController controller;
@@ -100,16 +106,16 @@ public class SlideControlsOverlay extends JPanel {
             putValue(Action.NAME, I18n.tr("Download"));
             putValue(LimeAction.SHORT_NAME, I18n.tr("Download"));
             putValue(Action.SHORT_DESCRIPTION, I18n.tr("Download") + " " + controller.getSlide().title);
-            putValue(LimeAction.ICON_NAME, "DOWNLOAD_FILE_MORE_SOURCES");
+            putValue(LimeAction.ICON_NAME, "SLIDE_CONTROLS_OVERLAY_DOWNLOAD");
 
         }
 
         @Override
-        public void actionPerformed(ActionEvent arg0) {
+        public void actionPerformed(ActionEvent e) {
             controller.downloadSlide();
         }
     }
-    
+
     private static final class PreviewVideoAction extends AbstractAction {
 
         private SlidePanelController controller;
@@ -119,17 +125,16 @@ public class SlideControlsOverlay extends JPanel {
             putValue(Action.NAME, I18n.tr("Video Preview"));
             putValue(LimeAction.SHORT_NAME, I18n.tr("Video Preview"));
             putValue(Action.SHORT_DESCRIPTION, I18n.tr("Play Video preview of") + " " + controller.getSlide().title);
-            putValue(LimeAction.ICON_NAME, "DOWNLOAD_FILE_MORE_SOURCES");
+            putValue(LimeAction.ICON_NAME, "SLIDE_CONTROLS_OVERLAY_PREVIEW");
 
         }
 
         @Override
-        public void actionPerformed(ActionEvent arg0) {
+        public void actionPerformed(ActionEvent e) {
             controller.previewVideo();
         }
     }
 
-    
     private static final class PreviewAudioAction extends AbstractAction {
 
         private SlidePanelController controller;
@@ -139,12 +144,12 @@ public class SlideControlsOverlay extends JPanel {
             putValue(Action.NAME, I18n.tr("Audio Preview"));
             putValue(LimeAction.SHORT_NAME, I18n.tr("Audio Preview"));
             putValue(Action.SHORT_DESCRIPTION, I18n.tr("Play Audio preview of") + " " + controller.getSlide().title);
-            putValue(LimeAction.ICON_NAME, "DOWNLOAD_FILE_MORE_SOURCES");
+            putValue(LimeAction.ICON_NAME, "SLIDE_CONTROLS_OVERLAY_PREVIEW");
 
         }
 
         @Override
-        public void actionPerformed(ActionEvent arg0) {
+        public void actionPerformed(ActionEvent e) {
             controller.previewAudio();
         }
     }
