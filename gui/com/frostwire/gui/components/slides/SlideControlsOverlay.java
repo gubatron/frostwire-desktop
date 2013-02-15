4c955cb31efa801ded1a409ef3bc1b6191423f51
Alden Torres <aldenml@gmail.com>
2013-02-15 10:32:12 -0500

    Fixed slide overlay buttons foreground.

--- gui/com/frostwire/gui/components/slides/SlideControlsOverlay.java
+++ gui/com/frostwire/gui/components/slides/SlideControlsOverlay.java
@@ -5,16 +5,14 @@ import java.awt.Color;
 import java.awt.Composite;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 
-import javax.swing.AbstractButton;
 import javax.swing.Action;
 import javax.swing.JPanel;
 
 import net.miginfocom.swing.MigLayout;
 
-import org.pushingpixels.substance.internal.ui.SubstanceButtonUI;
+import org.pushingpixels.substance.internal.utils.SubstanceTextUtilities;
 
 import com.limegroup.gnutella.gui.I18n;
 import com.limegroup.gnutella.gui.IconButton;
@@ -25,6 +23,7 @@ class SlideControlsOverlay extends JPanel {
 
     private static final Color BACKGROUND = new Color(45, 52, 58); // 2d343a
     private static final float BACKGROUND_ALPHA = 0.7f;
+    private static final Color BUTTONS_FOREGROUND = new Color(255, 255, 255);
 
     private final SlidePanelController controller;
     private final Composite overlayComposite;
@@ -48,20 +47,20 @@ class SlideControlsOverlay extends JPanel {
         final Slide slide = controller.getSlide();
 
         if (slide.hasFlag(Slide.SHOW_PREVIEW_BUTTONS_ON_THE_LEFT)) {
-            addPreviewButtons(slide, "cell 1 1","cell 2 1");
-            addDownloadInstallButton(slide,"cell 3 1");
+            addPreviewButtons(slide, "cell 1 1", "cell 2 1");
+            addDownloadInstallButton(slide, "cell 3 1");
         } else {
-            addDownloadInstallButton(slide,"cell 1 1");
-            addPreviewButtons(slide, "cell 2 1","cell 3 1");
+            addDownloadInstallButton(slide, "cell 1 1");
+            addPreviewButtons(slide, "cell 2 1", "cell 3 1");
         }
     }
 
     private void addPreviewButtons(final Slide slide, String constraintVideoPreview, String constraintAudioPreview) {
         if (slide.hasFlag(Slide.SHOW_VIDEO_PREVIEW_BUTTON)) {
             //add video preview button
-            add(new OverlayIconButton(new PreviewVideoAction(controller)),constraintVideoPreview);
+            add(new OverlayIconButton(new PreviewVideoAction(controller)), constraintVideoPreview);
         }
-      
+
         if (slide.hasFlag(Slide.SHOW_AUDIO_PREVIEW_BUTTON)) {
             //add audio preview button
             add(new OverlayIconButton(new PreviewAudioAction(controller)), constraintAudioPreview);
@@ -70,7 +69,7 @@ class SlideControlsOverlay extends JPanel {
 
     private void addDownloadInstallButton(final Slide slide, String constraints) {
         if (slide.method == Slide.SLIDE_DOWNLOAD_METHOD_HTTP || slide.method == Slide.SLIDE_DOWNLOAD_METHOD_TORRENT) {
-      
+
             if (slide.hasFlag(Slide.POST_DOWNLOAD_EXECUTE)) {
                 //add install button
                 add(new OverlayIconButton(new InstallAction(controller)), constraints);// "cell column row width height"
@@ -144,7 +143,7 @@ class SlideControlsOverlay extends JPanel {
             putValue(LimeAction.SHORT_NAME, I18n.tr("Video Preview"));
             putValue(Action.SHORT_DESCRIPTION, I18n.tr("Play Video preview of") + " " + controller.getSlide().title);
             putValue(LimeAction.ICON_NAME, "SLIDE_CONTROLS_OVERLAY_PREVIEW");
-            putValue(LimeAction.ICON_NAME_ROLLOVER,"SLIDE_CONTROLS_OVERLAY_PREVIEW_ROLLOVER");
+            putValue(LimeAction.ICON_NAME_ROLLOVER, "SLIDE_CONTROLS_OVERLAY_PREVIEW_ROLLOVER");
         }
 
         @Override
@@ -171,17 +170,13 @@ class SlideControlsOverlay extends JPanel {
             controller.previewAudio();
         }
     }
-    
+
     private static final class OverlayIconButton extends IconButton {
 
         public OverlayIconButton(Action action) {
             super(action);
-            setOpaque(false);
-            setUI(new SubstanceButtonUI(this) {
-                protected void paintButtonText(Graphics g, AbstractButton button, Rectangle textRect, String text) {
-
-                };
-            });
+            putClientProperty(SubstanceTextUtilities.ENFORCE_FG_COLOR, Boolean.TRUE);
+            setForeground(BUTTONS_FOREGROUND);
         }
     }
 }
