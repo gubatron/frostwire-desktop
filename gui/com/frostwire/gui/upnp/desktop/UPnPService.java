cad798893d7d595a1b01831a8fd6379da8a5d255
Alden Torres <aldenml@gmail.com>
2013-04-15 15:17:30 -0400

    Backporting of cling executor creation from android.

--- gui/com/frostwire/gui/upnp/desktop/UPnPService.java
+++ gui/com/frostwire/gui/upnp/desktop/UPnPService.java
@@ -130,7 +130,17 @@ public class UPnPService implements Runnable {
     }
 
     protected ExecutorService createFrostWireExecutor() {
-        return new ThreadPoolExecutor(0,32,30,TimeUnit.SECONDS,new SynchronousQueue<Runnable>(),new ClingThreadFactory());
+        return new ThreadPoolExecutor(0, 32, 30, TimeUnit.SECONDS, new SynchronousQueue<Runnable>(), new ClingThreadFactory()) {
+            @Override
+            public void execute(Runnable command) {
+                try {
+                    super.execute(command);
+                } catch (Throwable e) {
+                    //gubatron: we're catching a RejectedExecutionException until we figure out a solution.
+                    //we're probably being too aggresive submitting tasks in the first place.
+                }
+            }
+        };
     }
 
     private static LocalDevice createLocalDevice() {
