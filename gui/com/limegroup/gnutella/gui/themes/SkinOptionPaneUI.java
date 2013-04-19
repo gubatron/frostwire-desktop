19e793309a18b97fb05d548d33401a492205fecd
aldenml <aldenml@gmail.com>
2013-04-19 16:03:55 -0400

    Sking based solution of unicode characters in JOptionPane.

--- gui/com/limegroup/gnutella/gui/themes/SkinOptionPaneUI.java
+++ gui/com/limegroup/gnutella/gui/themes/SkinOptionPaneUI.java
@@ -18,9 +18,15 @@
 
 package com.limegroup.gnutella.gui.themes;
 
+import java.awt.Container;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+
 import javax.swing.JComponent;
+import javax.swing.JLabel;
 import javax.swing.plaf.ComponentUI;
 
+import org.limewire.util.OSUtils;
 import org.pushingpixels.substance.internal.ui.SubstanceOptionPaneUI;
 
 /**
@@ -33,5 +39,41 @@ public class SkinOptionPaneUI extends SubstanceOptionPaneUI {
 
     public static ComponentUI createUI(JComponent comp) {
         return ThemeMediator.CURRENT_THEME.createOptionPaneUI(comp);
+    }
+
+    @Override
+    public void installUI(JComponent c) {
+        super.installUI(c);
+    }
+
+    @Override
+    protected void addMessageComponents(Container container, GridBagConstraints cons, Object msg, int maxll, boolean internallyCreated) {
+        super.addMessageComponents(container, cons, msg, maxll, internallyCreated);
+
+        if (msg instanceof JLabel) {
+            fixLabelFont((JLabel) msg);
+        }
+    }
+
+    private void fixLabelFont(JLabel label) {
+        if (OSUtils.isWindows()) {
+            Font currentFont = label.getFont();
+            if (!canDisplayMessage(currentFont)) {
+                label.setFont(new Font("Dialog", Font.PLAIN, 12));
+            }
+        }
+    }
+
+    private boolean canDisplayMessage(Font f) {
+        boolean result = true;
+
+        Object msg = getMessage();
+
+        if (msg instanceof String) {
+            String s = (String) msg;
+            result = f.canDisplayUpTo(s) == -1;
+        }
+
+        return result;
     }
 }
