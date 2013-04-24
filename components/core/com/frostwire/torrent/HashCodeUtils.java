283cb5d7685e3721b48122935741e6c149e62a61
Alden Torres <aldenml@gmail.com>
2013-04-24 10:09:51 -0400

    New attempt for torrent vuze code fork.

---
+++ components/core/com/frostwire/torrent/HashCodeUtils.java
@@ -0,0 +1,48 @@
+/*
+ * Created by Joseph Bridgewater
+ * Created on Feb 24, 2006
+ * Copyright (C) 2005, 2006 Aelitis, All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * AELITIS, SAS au capital de 46,603.30 euros
+ * 8 Allee Lenotre, La Grille Royale, 78600 Le Mesnil le Roi, France.
+ *
+ */
+
+package com.frostwire.torrent;
+
+/**
+ * @author MjrTom Feb 24, 2006
+ * This is for utilities for calculating custom object hashCode() values
+ */
+public final class HashCodeUtils
+{
+    /**
+     * bob jenkin's hash function 
+     */
+    public static final int hashCode(byte[] array)
+    {
+    	int hash = 0;
+        for (int i = 0; i < array.length; i++) {
+            hash += array[i];
+            hash += (hash << 10);
+            hash ^= (hash >> 6);
+        }
+        hash += (hash << 3);
+        hash ^= (hash >> 11);
+        hash += (hash << 15);
+        return hash;
+    }
+    
+}
