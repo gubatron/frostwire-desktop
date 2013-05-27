3486f5b13bbe7e76f4824c5314bfac1600c6c8eb
Alden Torres <aldenml@gmail.com>
2013-05-27 18:50:47 -0400

    Fixed rendering of common controls.

--- gui/com/limegroup/gnutella/gui/SizedTextField.java
+++ gui/com/limegroup/gnutella/gui/SizedTextField.java
@@ -1,3 +1,18 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.limegroup.gnutella.gui;
 
 import java.awt.Dimension;
@@ -14,14 +29,9 @@ import com.limegroup.gnutella.gui.GUIUtils.SizePolicy;
 public final class SizedTextField extends LimeTextField {
 	
 	/**
-     * 
-     */
-    private static final long serialVersionUID = 8153967547511971533L;
-
-    /**
 	 * Constant for the standard height for <tt>JTextField</tt>.
 	 */
-	public static final int STANDARD_HEIGHT = 20;	
+	public static final int STANDARD_HEIGHT = 28;	
 	
 	/**
 	 * Constant for the standard <tt>Dimension</tt> for <tt>JTextField</tt>.
@@ -85,5 +95,4 @@ public final class SizedTextField extends LimeTextField {
 
 		GUIUtils.restrictSize(this, sizePolicy);
 	}
-
 }
