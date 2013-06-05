91ee61d827f6e23754597127a75c0046522959b9
gubatron <gubatron@gmail.com>
2013-06-04 20:19:22 -0400

    missing illegal character

--- components/common/src/main/java/org/limewire/util/FilenameUtils.java
+++ components/common/src/main/java/org/limewire/util/FilenameUtils.java
@@ -95,7 +95,7 @@ public class FilenameUtils {
      * Sorted list of invalid filename character keycodes.
      * @author gubatron
      */
-    public final static int[] illegalChars = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 34, 58, 42, 47, 60, 62, 63, 92, 124};
+    public final static int[] illegalChars = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 34, 58, 42, 47, 58, 60, 62, 63, 92, 124};
 
 
     /**
