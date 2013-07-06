94a29fc4adbaae4710489110ee1decd85dbd5427
gubatron <gubatron@gmail.com>
2013-07-06 17:02:46 -0400

    padding within the search field. Issue 1 on 4018284-frostwire

--- gui/com/limegroup/gnutella/gui/ApplicationHeader.java
+++ gui/com/limegroup/gnutella/gui/ApplicationHeader.java
@@ -24,6 +24,7 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
@@ -162,6 +163,7 @@ public final class ApplicationHeader extends JPanel implements RefreshListener {
         Font origFont = cloudSearchField.getFont();
         Font newFont = origFont.deriveFont(origFont.getSize2D() + 2f);
         cloudSearchField.setFont(newFont);
+        cloudSearchField.setMargin(new Insets(0,2,0,0));
     }
 
     private void createUpdateButton() {
