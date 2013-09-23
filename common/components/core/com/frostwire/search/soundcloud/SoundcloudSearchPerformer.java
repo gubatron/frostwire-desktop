35cfa3326b93df5e5566006da60e9da284bfb025
Alden Torres <aldenml@gmail.com>
2013-09-23 14:28:28 -0400

    Changing Soundcloud classes.

--- common/components/core/com/frostwire/search/soundcloud/SoundcloudSearchPerformer.java
+++ common/components/core/com/frostwire/search/soundcloud/SoundcloudSearchPerformer.java
@@ -19,11 +19,13 @@
 package com.frostwire.search.soundcloud;
 
 import java.text.SimpleDateFormat;
+import java.util.List;
 import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import com.frostwire.search.PagedRegexSearchPerformer;
+import com.frostwire.search.CrawlPagedWebSearchPerformer;
+import com.frostwire.search.SearchResult;
 import com.frostwire.util.JsonUtils;
 
 /**
@@ -31,7 +33,7 @@ import com.frostwire.util.JsonUtils;
  * @author aldenml
  *
  */
-public class SoundcloudSearchPerformer extends PagedRegexSearchPerformer<SoundcloudSearchResult> {
+public class SoundcloudSearchPerformer extends CrawlPagedWebSearchPerformer<SoundcloudSearchResult> {
 
     private static final int MAX_RESULTS = 16;
 
@@ -41,39 +43,45 @@ public class SoundcloudSearchPerformer extends PagedRegexSearchPerformer<Soundcl
     private static final Pattern PATTERN = Pattern.compile(REGEX);
 
     public SoundcloudSearchPerformer(long token, String keywords, int timeout) {
-        super(token, keywords, timeout, MAX_RESULTS / 4, MAX_RESULTS);
+        super(token, keywords, timeout, 1, MAX_RESULTS);
     }
 
-    @Override
-    public Pattern getPattern() {
-        return PATTERN;
-    }
-
-    @Override
-    public SoundcloudSearchResult fromMatcher(Matcher matcher) {
-        SoundcloudItem item = JsonUtils.toObject(matcher.group(3), SoundcloudItem.class);
-        try {
-            item.thumbnailUrl = buildThumbnailUrl(matcher.group(1));
-            item.date = new SimpleDateFormat(DATE_FORMAT, Locale.US).parse(matcher.group(2)).getTime();
-        } catch (Throwable e) {
-            item.date = -1;
-        }
-        return new SoundcloudSearchResult(item);
-    }
-
-    @Override
-    public String fetch(String url) {
-        return fetch(url, "web-ver=1", null);
-    }
+//    @Override
+//    public SoundcloudSearchResult fromMatcher(Matcher matcher) {
+//        SoundcloudItem item = JsonUtils.toObject(matcher.group(3), SoundcloudItem.class);
+//        try {
+//            item.thumbnailUrl = buildThumbnailUrl(matcher.group(1));
+//            item.date = new SimpleDateFormat(DATE_FORMAT, Locale.US).parse(matcher.group(2)).getTime();
+//        } catch (Throwable e) {
+//            item.date = -1;
+//        }
+//        return new SoundcloudSearchResult(item);
+//    }
 
     @Override
     protected String getUrl(int page, String encodedKeywords) {
-        return "http://soundcloud.com/tracks/search?page=" + page + "&q[fulltext]=" + encodedKeywords + "&q[downloadable]=true&advanced=1";
+        return "https://api.sndcdn.com/search/sounds?q=" +encodedKeywords + "&limit=30&offset=0&client_id=b45b1aa10f1ac2941910a7f0d10f8e28";
     }
 
     private String buildThumbnailUrl(String str) {
         //http://i1.sndcdn.com/artworks-000019588274-le8r71-crop.jpg?be0edad
         //https://i1.sndcdn.com/artworks-000019588274-le8r71-t500x500.jpg
         return "http://i1.sndcdn.com/artworks-" + str.substring(0, str.indexOf("-crop.")) + "-t300x300.jpg";
+    }
+
+    @Override
+    protected String getCrawlUrl(SoundcloudSearchResult sr) {
+        return null;
+    }
+
+    @Override
+    protected List<? extends SearchResult> crawlResult(SoundcloudSearchResult sr, byte[] data) throws Exception {
+        return null;
+    }
+
+    @Override
+    protected List<? extends SearchResult> searchPage(String page) {
+        // TODO Auto-generated method stub
+        return null;
     }
 }
