f25fff6276f9439ea94b8f94d22c23262c620c8f
gubatron <gubatron@gmail.com>
2013-10-25 13:35:09 -0400

    All Headers, and be done with it for 2014 :)

--- gui/com/frostwire/gui/library/DeviceUploadDialog.java
+++ gui/com/frostwire/gui/library/DeviceUploadDialog.java
@@ -8,7 +8,7 @@ import java.awt.Insets;
 import java.awt.event.ActionEvent;
 /*
  * Created by Angel Leon (@gubatron), Alden Torres (aldenml)
- * Copyright (c) 2011, 2012, FrostWire(R). All rights reserved.
+ * Copyright (c) 2011-2014, FrostWire(R). All rights reserved.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- gui/com/frostwire/gui/mplayer/MPlayerComponent.java
+++ gui/com/frostwire/gui/mplayer/MPlayerComponent.java
@@ -1,6 +1,6 @@
 /*
  * Created by Angel Leon (@gubatron), Alden Torres (aldenml), Erich Pleny (erichpleny)
- * Copyright (c) 2011, 2012, FrostWire(R). All rights reserved.
+ * Copyright (c) 2011-2014, FrostWire(R). All rights reserved.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
