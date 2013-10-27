029f83077d43f72890315e408fa16b4bd4645f88
Alden Torres <aldenml@gmail.com>
2013-10-27 13:29:51 -0400

    DRY refactor.

--- components/core/com/frostwire/search/torlock/TorLockSearchPerformer.java
+++ components/core/com/frostwire/search/torlock/TorLockSearchPerformer.java
@@ -18,16 +18,10 @@
 
 package com.frostwire.search.torlock;
 
-import java.util.LinkedList;
-import java.util.List;
 import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-import com.frostwire.search.CrawlRegexSearchPerformer;
 import com.frostwire.search.CrawlableSearchResult;
-import com.frostwire.search.PerformersHelper;
-import com.frostwire.search.SearchResult;
-import com.frostwire.search.torrent.TorrentCrawlableSearchResult;
+import com.frostwire.search.torrent.TorrentRegexSearchPerformer;
 
 /**
  * 
@@ -35,23 +29,19 @@ import com.frostwire.search.torrent.TorrentCrawlableSearchResult;
  * @author aldenml
  *
  */
-public class TorLockSearchPerformer extends CrawlRegexSearchPerformer<CrawlableSearchResult> {
+public class TorLockSearchPerformer extends TorrentRegexSearchPerformer<TorLockSearchResult> {
 
     private static final int MAX_RESULTS = 10;
+    private static final String REGEX = "(?is)<a href=/torrent/([0-9]*?/.*?\\.html)>";
+    private static final String HTML_REGEX = "(?is).*?<td><b>Name:</b></td><td>(.*?).torrent</td>.*?<td><b>Size:</b></td><td>(.*?) in .*? files</td>.*?<td><b>Added:</b></td><td>Uploaded on (.*?) by .*?</td>.*?<font color=#FF5400><b>(.*?)</b></font> seeders.*?<td align=center><a href=\"/tor/(.*?).torrent\"><img src=http://www.torlock.com/images/dlbutton2.png></a></td>.*?";
 
     public TorLockSearchPerformer(long token, String keywords, int timeout) {
-        super(token, keywords, timeout, 1, 2 * MAX_RESULTS, MAX_RESULTS);
+        super(token, keywords, timeout, 1, 2 * MAX_RESULTS, MAX_RESULTS, REGEX, HTML_REGEX);
     }
 
-    private static final String REGEX = "(?is)<a href=/torrent/([0-9]*?/.*?\\.html)>";
-    private static final Pattern PATTERN = Pattern.compile(REGEX);
-
-    private static final String HTML_REGEX = "(?is).*?<td><b>Name:</b></td><td>(.*?).torrent</td>.*?<td><b>Size:</b></td><td>(.*?) in .*? files</td>.*?<td><b>Added:</b></td><td>Uploaded on (.*?) by .*?</td>.*?<font color=#FF5400><b>(.*?)</b></font> seeders.*?<td align=center><a href=\"/tor/(.*?).torrent\"><img src=http://www.torlock.com/images/dlbutton2.png></a></td>.*?";
-    private static final Pattern HTML_PATTERN = Pattern.compile(HTML_REGEX);
-
     @Override
-    public Pattern getPattern() {
-        return PATTERN;
+    protected String getUrl(int page, String encodedKeywords) {
+        return "http://www.torlock.com/?sort=seeds&c=&search=Search&q=" + encodedKeywords;
     }
 
     @Override
@@ -61,39 +51,7 @@ public class TorLockSearchPerformer extends CrawlRegexSearchPerformer<CrawlableS
     }
 
     @Override
-    protected String getUrl(int page, String encodedKeywords) {
-        return "http://www.torlock.com/?sort=seeds&c=&search=Search&q=" + encodedKeywords;
-    }
-
-    @Override
-    protected String getCrawlUrl(CrawlableSearchResult sr) {
-        String crawlUrl = null;
-
-        if (sr instanceof TorLockTempSearchResult) {
-            crawlUrl = sr.getDetailsUrl();
-        } else if (sr instanceof TorLockSearchResult) {
-            crawlUrl = ((TorLockSearchResult) sr).getTorrentUrl();
-        }
-
-        return crawlUrl;
-    }
-
-    @Override
-    protected List<? extends SearchResult> crawlResult(CrawlableSearchResult sr, byte[] data) throws Exception {
-        List<SearchResult> list = new LinkedList<SearchResult>();
-
-        if (sr instanceof TorLockTempSearchResult) {
-            String html = new String(data, "UTF-8");
-
-            Matcher matcher = HTML_PATTERN.matcher(html);
-
-            if (matcher.find()) {
-                list.add(new TorLockSearchResult(sr.getDetailsUrl(), matcher));
-            }
-        } else if (sr instanceof TorLockSearchResult) {
-            list.addAll(PerformersHelper.crawlTorrent(this, (TorrentCrawlableSearchResult) sr, data));
-        }
-
-        return list;
+    protected TorLockSearchResult fromHtmlMatcher(CrawlableSearchResult sr, Matcher matcher) {
+        return new TorLockSearchResult(sr.getDetailsUrl(), matcher);
     }
 }
