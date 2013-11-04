7f488e2a9f6bb3eff287b8c5d3ba335d02d7c25b
gubatron <gubatron@gmail.com>
2013-11-04 13:58:30 -0500

    search engine icon, build bump

--- core/com/limegroup/gnutella/util/FrostWireUtils.java
+++ core/com/limegroup/gnutella/util/FrostWireUtils.java
@@ -45,7 +45,7 @@ public final class FrostWireUtils {
 	private static final String FROSTWIRE_VERSION = "5.6.7";
 	
 	/** Build number for the current version, gets reset to 1 on every version bump*/
-	private static final int BUILD_NUMBER = 1;
+	private static final int BUILD_NUMBER = 2;
 
 	/**
 	 * Make sure the constructor can never be called.
