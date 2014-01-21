c3ad89ffd476d7e9403b62a2d0c9c8d5c1c0bc56
Alden Torres <aldenml@gmail.com>
2014-01-21 14:21:16 -0500

    Disabled DHT on android.

--- components/core/com/frostwire/vuze/VuzeManager.java
+++ components/core/com/frostwire/vuze/VuzeManager.java
@@ -18,20 +18,16 @@
 
 package com.frostwire.vuze;
 
-import java.util.List;
-
 import org.gudy.azureus2.core3.config.COConfigurationManager;
-import org.gudy.azureus2.core3.download.DownloadManager;
-import org.gudy.azureus2.core3.util.AERunStateHandler;
+import org.gudy.azureus2.plugins.PluginManager;
+import org.gudy.azureus2.plugins.PluginManagerDefaults;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.aelitis.azureus.core.AzureusCore;
-import com.aelitis.azureus.core.dht.DHT;
 import com.aelitis.azureus.core.dht.speed.DHTSpeedTester;
 import com.aelitis.azureus.core.dht.speed.DHTSpeedTesterListener;
-import com.aelitis.azureus.plugins.dht.DHTPlugin;
-import com.frostwire.util.Condition;
+import com.frostwire.util.OSUtils;
 
 /**
  * Class to initialize the azureus core.
@@ -51,6 +47,18 @@ public final class VuzeManager {
 
         COConfigurationManager.setParameter("network.max.simultaneous.connect.attempts", 1);
 
+        disableDefaultPlugins();
+
+        if (OSUtils.isAndroid()) {
+            disableSpeedManager();
+        }
+    }
+
+    public AzureusCore getCore() {
+        return core;
+    }
+
+    private void disableSpeedManager() {
         core.getSpeedManager().setEnabled(false);
         DHTSpeedTester oldTester = core.getSpeedManager().getSpeedTester();
 
@@ -58,135 +66,54 @@ public final class VuzeManager {
 
             @Override
             public void setContactNumber(int number) {
-                // TODO Auto-generated method stub
-
             }
 
             @Override
             public void removeListener(DHTSpeedTesterListener listener) {
-                // TODO Auto-generated method stub
-
             }
 
             @Override
             public int getContactNumber() {
-                // TODO Auto-generated method stub
                 return 0;
             }
 
             @Override
             public void destroy() {
-                // TODO Auto-generated method stub
-
             }
 
             @Override
             public void addListener(DHTSpeedTesterListener listener) {
-                // TODO Auto-generated method stub
-
             }
         });
 
         if (oldTester != null) {
             oldTester.destroy();
         }
-
-        new ActivityMonitor().start();
     }
 
-    public AzureusCore getCore() {
-        return core;
-    }
+    private static void disableDefaultPlugins() {
+        PluginManagerDefaults pmd = PluginManager.getDefaults();
 
-    public boolean isDHTSleeping() {
-        return AERunStateHandler.isDHTSleeping();
-    }
+        pmd.setDefaultPluginEnabled(PluginManagerDefaults.PID_START_STOP_RULES, false);
+        pmd.setDefaultPluginEnabled(PluginManagerDefaults.PID_REMOVE_RULES, false);
+        pmd.setDefaultPluginEnabled(PluginManagerDefaults.PID_SHARE_HOSTER, false);
+        pmd.setDefaultPluginEnabled(PluginManagerDefaults.PID_DEFAULT_TRACKER_WEB, false);
 
-    public void setDHTSleeping(boolean sleeping) {
-        if (sleeping != isDHTSleeping()) {
-            long rm = AERunStateHandler.getResourceMode();
+        pmd.setDefaultPluginEnabled(PluginManagerDefaults.PID_PLUGIN_UPDATE_CHECKER, false);
+        pmd.setDefaultPluginEnabled(PluginManagerDefaults.PID_CORE_UPDATE_CHECKER, false);
+        pmd.setDefaultPluginEnabled(PluginManagerDefaults.PID_CORE_PATCH_CHECKER, false);
+        pmd.setDefaultPluginEnabled(PluginManagerDefaults.PID_PLATFORM_CHECKER, false);
 
-            if (sleeping) {
-                AERunStateHandler.setResourceMode(rm | AERunStateHandler.RS_DHT_SLEEPING);
-            } else {
-                AERunStateHandler.setResourceMode(rm & ~AERunStateHandler.RS_DHT_SLEEPING);
-            }
+        pmd.setDefaultPluginEnabled(PluginManagerDefaults.PID_BUDDY, false);
+        pmd.setDefaultPluginEnabled(PluginManagerDefaults.PID_RSS, false);
 
-            suspendedDHTs(sleeping);
-        }
-    }
-
-    private void suspendedDHTs(boolean suspended) {
-
-        DHTPlugin plugin = (DHTPlugin) core.getPluginManager().getPluginInterfaceByClass(DHTPlugin.class);
-
-        for (DHT dht : plugin.getDHTs()) {
-            dht.setSuspended(suspended);
-        }
-    }
-
-    private final class ActivityMonitor extends Thread {
-
-        private boolean running;
-
-        public ActivityMonitor() {
-            super("Vuze Activity Monitor");
-
-            this.setDaemon(true);
-
-            Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
-
-                @Override
-                public void run() {
-                    running = false;
-                }
-            }));
-        }
-
-        @Override
-        public void run() {
-            running = true;
-
-            while (running) {
-                sleep();
-                checkActivity();
-            }
-        }
-
-        private void sleep() {
-            try {
-                Thread.sleep(30 * 1000);
-            } catch (InterruptedException e) {
-            }
-        }
-
-        private void checkActivity() {
-            try {
-                if (isDownloading()) {
-                    setDHTSleeping(false);
-                } else {
-                    setDHTSleeping(true);
-                }
-            } catch (Throwable e) {
-                LOG.error("Error checking vuze activity", e);
-            }
-        }
-
-        private boolean isDownloading() {
-            List<DownloadManager> dms = core.getGlobalManager().getDownloadManagers();
-
-            boolean downloading = false;
-
-            for (DownloadManager dm : dms) {
-
-                int state = dm.getState();
-
-                if (Condition.in(state, DownloadManager.STATE_ALLOCATING, DownloadManager.STATE_DOWNLOADING, DownloadManager.STATE_WAITING)) {
-                    downloading = true;
-                }
-            }
-
-            return downloading;
+        if (OSUtils.isAndroid()) {
+            pmd.setDefaultPluginEnabled(PluginManagerDefaults.PID_DHT, false);
+            pmd.setDefaultPluginEnabled(PluginManagerDefaults.PID_DHT_TRACKER, false);
+            pmd.setDefaultPluginEnabled(PluginManagerDefaults.PID_MAGNET, false);
+            pmd.setDefaultPluginEnabled(PluginManagerDefaults.PID_EXTERNAL_SEED, false);
+            pmd.setDefaultPluginEnabled(PluginManagerDefaults.PID_LOCAL_TRACKER, false);
+            pmd.setDefaultPluginEnabled(PluginManagerDefaults.PID_TRACKER_PEER_AUTH, false);
         }
     }
 }
