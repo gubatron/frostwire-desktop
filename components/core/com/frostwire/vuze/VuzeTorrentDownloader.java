07b443f3d4606139daab7fe860867371e051b447
Alden Torres <aldenml@gmail.com>
2014-02-03 14:27:32 -0500

    Creating vuze torrent downloader abstraction.

---
+++ components/core/com/frostwire/vuze/VuzeTorrentDownloader.java
@@ -0,0 +1,67 @@
+/*
+ * Created by Angel Leon (@gubatron), Alden Torres (aldenml)
+ * Copyright (c) 2011-2014, FrostWire(R). All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.frostwire.vuze;
+
+import org.gudy.azureus2.core3.torrentdownloader.TorrentDownloader;
+import org.gudy.azureus2.core3.torrentdownloader.TorrentDownloaderCallBackInterface;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 
+ * @author gubatron
+ * @author aldenml
+ *
+ */
+public final class VuzeTorrentDownloader {
+
+    private static final Logger LOG = LoggerFactory.getLogger(VuzeTorrentDownloader.class);
+
+    private final TorrentDownloader dl;
+
+    private VuzeTorrentDownloadListener listener;
+
+    public VuzeTorrentDownloader(TorrentDownloader dl) {
+        this.dl = dl;
+    }
+
+    public VuzeTorrentDownloadListener getListener() {
+        return listener;
+    }
+
+    public void setListener(VuzeTorrentDownloadListener listener) {
+        this.listener = listener;
+    }
+
+    private class TorrentDownloaderListener implements TorrentDownloaderCallBackInterface {
+
+        @Override
+        public void TorrentDownloaderEvent(int state, TorrentDownloader inf) {
+            if (listener != null) {
+                try {
+                    if (state == TorrentDownloader.STATE_FINISHED) {
+                        listener.onFinished(VuzeTorrentDownloader.this);
+                    }
+                } catch (Throwable e) {
+                    LOG.error("Error in client listener", e);
+                }
+            }
+        }
+    }
+}
