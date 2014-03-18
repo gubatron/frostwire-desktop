c562e3652ac76f86b4dba278fbb657c06cc5bce6
Alden Torres <aldenml@gmail.com>
2014-03-17 22:41:39 -0400

    Adding generic yes/no logic to abstract dialog.

--- components/core/com/frostwire/util/Ref.java
+++ components/core/com/frostwire/util/Ref.java
@@ -52,6 +52,6 @@ public final class Ref {
     }
 
     public static <T> boolean alive(Reference<T> ref) {
-        return ref.get() != null;
+        return ref != null && ref.get() != null;
     }
 }
