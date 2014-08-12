a3ad511717f6717eaf6f35855cd4274cfe6453e4
gubatron <gubatron@gmail.com>
2014-08-11 22:51:00 -0400

    5.7.5 (version bump)

--- core/com/limegroup/gnutella/util/FrostWireUtils.java
+++ core/com/limegroup/gnutella/util/FrostWireUtils.java
@@ -42,7 +42,7 @@ public final class FrostWireUtils {
 	/** 
 	 * Constant for the current version of FrostWire.
 	 */
-	private static final String FROSTWIRE_VERSION = "5.7.4";
+	private static final String FROSTWIRE_VERSION = "5.7.5";
 	
 	/** Build number for the current version, gets reset to 1 on every version bump*/
 	private static final int BUILD_NUMBER = 1;
