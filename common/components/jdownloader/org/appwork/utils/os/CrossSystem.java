afcf88906114816be5198893b7b10eda7fc8c2c3
Alden Torres <aldenml@gmail.com>
2014-09-24 13:35:09 -0400

    Removed internal copy of common.

--- common/components/jdownloader/org/appwork/utils/os/CrossSystem.java
+++
@@ -1,8 +0,0 @@
-package org.appwork.utils.os;
-
-public class CrossSystem {
-
-    public static boolean isWindows() {
-        return false;
-    }
-}
--- common/pull.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-git subtree pull --prefix=common -m "Merged frostwire-common changes" https://github.com/frostwire/frostwire-common.git master
