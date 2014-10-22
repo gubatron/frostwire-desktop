bc2c35341022e63907167028d40af2df6a7a643b
Alden Torres <aldenml@gmail.com>
2014-10-21 22:09:49 -0400

    Cleanup.

--- src/com/limegroup/gnutella/gui/LimeWireGUIModule.java
+++ src/com/limegroup/gnutella/gui/LimeWireGUIModule.java
@@ -12,23 +12,10 @@ public class LimeWireGUIModule {
         }
         return INSTANCE;
     }
-    
-    private final ActivityCallback activityCallback;
-    private final LocalClientInfoFactory localClientInfoFactory;
-    
+
     private LimeWireGUIModule() {
-        activityCallback = VisualConnectionCallback.instance();
-        localClientInfoFactory = LocalClientInfoFactoryImpl.instance();
     }
-    
-    public ActivityCallback getActivityCallback() {
-        return activityCallback;
-    }
-    
-    public LocalClientInfoFactory getLocalClientInfoFactory() {
-        return localClientInfoFactory;
-    }
-    
+
     public LimeWireGUI getLimeWireGUI() {
         return LimeWireGUI.instance();
     }
--- src/com/limegroup/gnutella/gui/LimeWireModule.java
+++ src/com/limegroup/gnutella/gui/LimeWireModule.java
@@ -1,7 +1,5 @@
 package com.limegroup.gnutella.gui;
 
-import com.limegroup.gnutella.LimeWireCoreModule;
-
 /** The master LimeWire module. */
 public class LimeWireModule {
     
@@ -14,18 +12,12 @@ public class LimeWireModule {
         return INSTANCE;
     }
     
-    private final LimeWireCoreModule limeWireCoreModule;
     private final LimeWireGUIModule limeWireGUIModule;
     
     private LimeWireModule() {
-        limeWireCoreModule = LimeWireCoreModule.instance(VisualConnectionCallback.instance());
         limeWireGUIModule = LimeWireGUIModule.instance();
     }
-    
-    public LimeWireCoreModule getLimeWireCoreModule() {
-        return limeWireCoreModule;
-    }
-    
+
     public LimeWireGUIModule getLimeWireGUIModule() {
         return limeWireGUIModule;
     }
