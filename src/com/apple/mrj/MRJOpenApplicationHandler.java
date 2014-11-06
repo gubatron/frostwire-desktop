05d4535795ddc08514e6ffcb96d084311fbdfa74
Alden Torres <aldenml@gmail.com>
2014-11-06 18:19:23 -0500

    More MRJ related cleanup.

--- src/com/apple/mrj/MRJOpenApplicationHandler.java
+++
@@ -1,6 +0,0 @@
-package com.apple.mrj;
-
-public abstract interface MRJOpenApplicationHandler
-{
-  public abstract void handleOpenApplication();
-}
--- src/com/apple/mrj/MRJPrefsHandler.java
+++
@@ -1,7 +0,0 @@
-package com.apple.mrj;
-
-public abstract interface MRJPrefsHandler
-{
-  public abstract void handlePrefs()
-    throws IllegalStateException;
-}
