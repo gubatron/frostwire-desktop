cceabe2db2686a3f10abf0615d11e42bcfbf1148
Alden Torres <aldenml@gmail.com>
2014-12-21 16:52:20 -0500

    Added date heuristic to solve the incomplete files problem.

--- src/com/frostwire/gui/bittorrent/BittorrentDownload.java
+++ src/com/frostwire/gui/bittorrent/BittorrentDownload.java
@@ -251,7 +251,7 @@ public class BittorrentDownload implements com.frostwire.gui.bittorrent.BTDownlo
         public void finished(BTDownload dl) {
             if (!SharingSettings.SEED_FINISHED_TORRENTS.getValue() || (dl.isPartial() && !SharingSettings.SEED_HANDPICKED_TORRENT_FILES.getValue())) {
                 dl.pause();
-                finalCleanup(dl.getIncompleteFiles(true));
+                finalCleanup(dl.getIncompleteFiles());
             }
 
             File saveLocation = dl.getSavePath();
