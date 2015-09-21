81352305d7aa62c5921e23725b60797e31cdd344
gubatron <gubatron@gmail.com>
2015-09-20 20:17:15 -0400

    license header fixes.

--- src/com/limegroup/gnutella/gui/search/SearchResultMediator.java
+++ src/com/limegroup/gnutella/gui/search/SearchResultMediator.java
@@ -1,4 +1,7 @@
 /*
+ * Created by Angel Leon (@gubatron), Alden Torres (aldenml)
+ * Copyright (c) 2011-2015, FrostWire(R). All rights reserved.
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -12,6 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 
 package com.limegroup.gnutella.gui.search;
 
