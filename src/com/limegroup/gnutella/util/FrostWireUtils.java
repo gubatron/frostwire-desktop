d8e6f2239d6da81bd3db6d68ce17924e79da3c9b
gubatron <gubatron@gmail.com>
2015-09-30 11:06:13 -0400

    build 2 - fixes XP compatibility issue.

--- src/com/limegroup/gnutella/util/FrostWireUtils.java
+++ src/com/limegroup/gnutella/util/FrostWireUtils.java
@@ -44,7 +44,7 @@ public final class FrostWireUtils {
     /**
      * Build number for the current version, gets reset to 1 on every version bump
      */
-    private static final int BUILD_NUMBER = 1;
+    private static final int BUILD_NUMBER = 2;
 
     /**
      * Make sure the constructor can never be called.
